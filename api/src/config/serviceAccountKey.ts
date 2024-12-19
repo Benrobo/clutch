5d4c89983ea45abfeb1c53e4eb10d469d2ab33d0
benrobo <alumonabenaiah71@gmail.com>
2024-12-19 09:25:26 +0100

    added major updates

--- api/src/config/serviceAccountKey.ts
+++ api/src/config/serviceAccountKey.ts
@@ -1,9 +1,10 @@
+import env from "./env.js";
+
 export default {
   type: "service_account",
   project_id: "nexusai-9f410",
   private_key_id: "e69b4c1659b64c77f8a675557e51f417e619e98f",
-  private_key:
-    "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC20mCbNOB/gu03\n0ro0CGslofUMUIpJUymUJvmGjIhszE8NxmuMTdHm2OZr9s8VT2a27N+Y9Tk3brLv\nFfOfLYlts+UJyvhhv2eCfn/ZBI2+GlErFyb7oCgyabmx+04EGNdQ+J605lZ1c/4p\nDCzApktgCD6/lIGKWcA6jMIlybn2OTJNPibYN72gjkdqZ2GYJxk556u+yjyNEpqv\nFcwo5YI0J1fXBBvsUMSlLMTHERzVLZltF5CmYeI7eUuFQwawDiRRFZnBAMh5sp9S\n91cI1tXuyY2HFElCsu/HyiS+8BOdqyTfbHG4115pcSkTvwAQZ3IpfWbM4WPoVOZv\nC9JbGLT/AgMBAAECggEAMHqf8Hg4kceDpPplsveKhhuo0d+nk+MQEv+0ykeIw6T3\nz5TOnxFhuD/K9lUnu6Rm/dnZPwPD7N2XaEvfYtXd6++4q/ydGbp6NcHaoA8TkRYE\ncRM+HJpvHlMn0TjtCqwVqJyM5TZxNcsQ2gym710Jfskg7odwRGIhVcVOlFumStKT\nSrCWS4reRp/2KTQyTs5b1z61v81TFP41EXhIKQ4YBq0f/GLr1TXevrjl3BWUHEoF\nQufxXXimSX9jGy4vTJbrbEHW39oO72yDpUSs5Y4yJnEONZANM0fnNcDBeQEsgxYh\nMP/2+Cf79Zkav2NKWvLCEEoVvkO9/7HxGTOiKuYvuQKBgQD1ioKEx+0xNGmR/dLu\nHDx3Kk//zGBiTdUWXVggzkwSd3iyM6c+jww9VMhT1BUrWK+6H2jABqnV0B+t4+Y1\nNSSdq/3qJVZ23xQpFhHeTu6WcMIDNcshC2P7pRuUKzATvLX0guQYBXsn4hBEZ8Yl\n4LGaG1fTrJSQZFAOxqxh8MdMyQKBgQC+m/LK/ctSJbYzd1fGSyAiIR0zEqLZDng2\nBbHy9KDysLYPxhHrFYwuGGX28tSUYcZ+nl450XFUS9w9b9kQ3sqe1nOkLnWMdDdn\nYpDXJ6aRSIYkb+/TS5G05MapPXgihZhQb5tU397BjMqHyVopIgBpTPqgXxVLX8gr\nSjtfYOv/hwKBgCXVS7sN2xxDySh3hbKuoxpxaJYvdYJwH68sftG/N7V4tx4uG0zm\nWapxj9Rqz0A4kAsLn+ZGoJCTWu4+OVBledjZkWwsiKAUS1TxU5t/af/ALAS2my/i\nQ2uYyyEPbn78CsU8Dc0tuwYwLPV9T+iyMGtgXy38X3/IFRXRwunsYakRAoGAWK7+\nO+vxWXsqVq/EKwJxJO/7QfP9yLeuvpPPw5aF++S0EIq7c4FHA3telZNNzOvhCrBs\nbsI/1FSnV/5eJDdEVrq4ez8SehLoZDW75h8QpK/1maZrEwgp9twqgjuTVZyKzYHf\n55FxduQmpx7L0C99uYCTa2JR6gvaeinDjhDuPKkCgYEApNauLFmgQ0aEw7RJ4rrW\nSEf/UWAnvo4uB72H0PVI3/j3P0zSeaOCvdRBdpljqed3VA9ROjMmw8hxT2KViSk7\ncXHFxjXDvqAdPvKjm4+gnJbgah38zwQSRjeUigdc4nV+5aavQBUefO19lubnoGLP\n9adYoLgaZktwGfgxvik1mr4=\n-----END PRIVATE KEY-----\n",
+  private_key: env.GOOGLE_SERVICE_ACCOUNT_KEY,
   client_email: "firebase-adminsdk-6hacl@nexusai-9f410.iam.gserviceaccount.com",
   client_id: "109960052018909899818",
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
