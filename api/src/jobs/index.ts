4900b9be6f391be1313d2a0f5f3d552ed88806af
benrobo <alumonabenaiah71@gmail.com>
2024-12-22 22:13:07 +0100

    added major updates

--- api/src/jobs/index.ts
+++ api/src/jobs/index.ts
@@ -1,9 +1,13 @@
 import { generateGameHighlightsMetadata } from "./generateHighlights.js";
 import { processGameHighlightsVideo } from "./processHighlightsVideo.js";
-import { processGamesEvery30Minutes } from "./scheduler.js";
+import {
+  processGamesEvery30Minutes,
+  processGameHighlightsVideoEvery30Minutes,
+} from "./scheduler.js";
 
 export const inngestFunctions = [
   processGamesEvery30Minutes,
+  processGameHighlightsVideoEvery30Minutes,
   processGameHighlightsVideo,
   generateGameHighlightsMetadata,
 ];
