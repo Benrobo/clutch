74ec43f06696f7e0497a0a1c63cbbdcc71e60396
benrobo <alumonabenaiah71@gmail.com>
2024-12-23 11:14:14 +0100

    added major updates

--- api/src/lib/schema-validator.ts
+++ api/src/lib/schema-validator.ts
@@ -1,1 +1,10 @@
 import * as zod from "zod";
+
+export const AddUserPreferencesSchema = zod.object({
+  teams: zod.array(zod.number()),
+  players: zod.array(zod.number()),
+});
+
+export const GetTeamRostersSchema = zod.object({
+  teamIds: zod.array(zod.number()).min(1, "At least one team ID is required"),
+});
