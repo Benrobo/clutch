040ab441b34981c74bb25af1a2d171d64ffbf6dc
benrobo <alumonabenaiah71@gmail.com>
2024-12-23 11:14:14 +0100

    added major updates

--- api/src/jobs/scheduler.ts
+++ api/src/jobs/scheduler.ts
@@ -14,7 +14,7 @@ export const processGamesEvery30Minutes = inngestClient.createFunction(
 export const processGameHighlightsVideoEvery30Minutes =
   inngestClient.createFunction(
     { id: "process-highlights-video-every-30min" },
-    { cron: "*/5 * * * *" },
+    { cron: "*/2 * * * *" },
     async ({ step }) => {
       inngestClient.send({
         name: "process-highlights-video",
