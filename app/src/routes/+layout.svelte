2f7e56bb982ceaa1c8b9a79fd1d392c9499ebc79
benrobo <alumonabenaiah71@gmail.com>
2024-12-23 21:08:51 +0100

    added major updates

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,6 +1,26 @@
 <script lang="ts">
-	import '../app.css';
-	let { children } = $props();
+	import queryClient from '$lib/config/tanstack-query';
+	import '../style/app.css';
+	import '../style/font.css';
+	import '@fontsource-variable/jetbrains-mono';
+	import '@fontsource/poppins/100.css';
+	import '@fontsource/poppins/200.css';
+	import '@fontsource/poppins/300.css';
+	import '@fontsource/poppins/400.css';
+	import '@fontsource/poppins/500.css';
+	import '@fontsource/poppins/600.css';
+	import '@fontsource/poppins/700.css';
+	import '@fontsource/bruno-ace/400.css';
+	import '@fontsource-variable/montserrat';
+	import { QueryClientProvider } from '@tanstack/svelte-query';
+	import { Toaster } from 'svelte-french-toast';
 </script>
 
-{@render children()}
+<body class="m-0 h-screen w-screen p-0">
+	<div class="m-0 h-screen w-full p-0">
+		<QueryClientProvider client={queryClient}>
+			<slot />
+		</QueryClientProvider>
+		<Toaster />
+	</div>
+</body>
