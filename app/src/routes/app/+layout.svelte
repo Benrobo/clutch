dfaf210115b71628e378aa6ce435bfd90f10ad04
benrobo <alumonabenaiah71@gmail.com>
2024-12-23 23:54:04 +0100

    added major updates

---
+++ app/src/routes/app/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import Flex from '@/components/Flex.svelte';
+</script>
+
+<div class="w-screen h-screen flex flex-center">
+	<div class="w-full h-full mx-auto md:max-w-[643px]">
+		<slot />
+	</div>
+</div>
---
+++ app/src/routes/app/onboarding/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import MainScreen from '@/modules/onboarding/components/MainScreen.svelte';
+</script>
+
+<MainScreen />
