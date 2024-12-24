cab8d99f34b6887ba5833d69736eaea2abdd3968
benrobo <alumonabenaiah71@gmail.com>
2024-12-24 14:53:15 +0100

    updated

--- app/src/routes/app/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import Flex from '@/components/Flex.svelte';
-</script>
-
-<div class="w-screen h-screen flex flex-center">
-	<div class="w-full h-full mx-auto md:max-w-[643px]">
-		<slot />
-	</div>
-</div>
