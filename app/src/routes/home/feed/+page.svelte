7ea29065e91ca5f9a80d9c39a4c39d966e0c528c
benrobo <alumonabenaiah71@gmail.com>
2024-12-24 14:53:07 +0100

    added major updates

--- app/src/routes/home/feed/+page.svelte
+++ app/src/routes/home/feed/+page.svelte
@@ -1,1 +1,30 @@
-<h1>Feed page</h1>
+<script lang="ts">
+	import Flex from '@/components/Flex.svelte';
+	import { sampleHighlights } from '@/data/highlights';
+	import HighlightVideo from '@/modules/discover/components/highlight-video.svelte';
+	import { onMount } from 'svelte';
+
+	const higlights = sampleHighlights;
+</script>
+
+<div class="fixed inset-0 flex items-center justify-center">
+	<div class="w-full h-full max-w-[600px] relative">
+		<div
+			class="w-full h-full highlight-videos-container overflow-y-auto snap-y snap-mandatory hideScrollBar3"
+		>
+			{#each higlights as hl}
+				<HighlightVideo {hl} />
+			{/each}
+		</div>
+	</div>
+</div>
+
+<style>
+	:global(body) {
+		overflow: hidden;
+	}
+
+	.highlight-videos-container {
+		/* scroll-padding-top: 20px; */
+	}
+</style>
