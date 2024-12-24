45c937716086a6b7fd89e8c3a13788b014358c37
benrobo <alumonabenaiah71@gmail.com>
2024-12-24 23:04:22 +0100

    added major updates

--- app/src/routes/home/+layout.svelte
+++ app/src/routes/home/+layout.svelte
@@ -1,13 +1,37 @@
 <script lang="ts">
 	import AuthLayout from '@/components/AuthLayout.svelte';
 	import BottomNavTab from '@/components/BottomNavTab.svelte';
+	import { useBrowser } from '$lib/hooks/useBrowser';
+	import { cn } from '$lib/utils';
+	import { afterUpdate, onMount } from 'svelte';
+
+	$: isSafariMobile = false;
+
+	const checkBrowser = () => {
+		const { isSafari, isMobile } = useBrowser();
+		isSafariMobile = isSafari && isMobile;
+	};
+
+	onMount(() => {
+		checkBrowser();
+		window.addEventListener('resize', checkBrowser);
+		return () => window.removeEventListener('resize', checkBrowser);
+	});
 </script>
 
-<!-- <AuthLayout> -->
-<div class="w-screen h-screen flex flex-center overflow-y-hidden">
-	<div class="w-full h-full mx-auto md:max-w-[643px]">
-		<slot />
-	</div>
+<div class="w-screen h-screen flex flex-col bg-dark-103">
+	<main class={cn('flex-1 overflow-hidden relative pb-[5em]', isSafariMobile && 'pbh-[10em]')}>
+		<div class="w-full h-full mx-auto md:max-w-[643px]">
+			<slot />
+		</div>
+	</main>
+	<BottomNavTab />
 </div>
-<BottomNavTab />
-<!-- </AuthLayout> -->
+
+<style>
+	:global(body) {
+		overflow: hidden;
+		background-color: #000;
+		height: 100%;
+	}
+</style>
