7a6112b069e25423573af08c95238c421cc5dc08
benrobo <alumonabenaiah71@gmail.com>
2024-12-24 14:53:07 +0100

    added major updates

---
+++ app/src/lib/data/highlights.ts
@@ -0,0 +1,254 @@
+import type { Highlights } from '../types/highlights';
+
+export const sampleHighlights: Highlights = [
+    {
+        "id": "ckweM7vgE2APVjfsesECj6",
+        "gameId": 747222,
+        "createdAt": "2024-12-23T18:12:00.352Z",
+        "likes": 0,
+        "thumbnail": {
+            "main": null,
+            "fallback": "https://img.mlbstatic.com/mlb-images/image/upload/w_1920,h_1080,f_jpg,c_fill,g_auto/mlb/qxmgfj2kpcjuzt3dvtgd.jpg"
+        },
+        "playback": {
+            "title": "Rockies vs. D-Backs Highlights",
+            "description": "Nolan Jones and the Rockies take on Corbin Carroll and the D-backs on March 31, 2024",
+            "mlbVideoUrl": "https://mlb-cuts-diamond.mlb.com/FORGE/2024/2024-03/31/1a925696-2ed17bb4-52d443a6-csvm-diamondx64-asset_1280x720_59_4000K.mp4",
+            "mlbVideoDuration": "00:03:14",
+            "processedVideoUrl": null,
+            "processedVideoDuration": null,
+            "orientation": "HORIZONTAL"
+        },
+        "game": {
+            "homeTeamId": 109,
+            "awayTeamId": 115,
+            "date": "2024-03-31T20:10:00Z",
+            "status": "Final"
+        }
+    },
+    {
+        "id": "9vBorHusUoj4k8FqpwmFaB",
+        "gameId": 745683,
+        "createdAt": "2024-12-23T18:11:59.345Z",
+        "likes": 0,
+        "thumbnail": {
+            "main": null,
+            "fallback": "https://img.mlbstatic.com/mlb-images/image/upload/w_1920,h_1080,f_jpg,c_fill,g_auto/v1711925262/mlb/vbdaa2jb8f0hfcvxbkhh.jpg"
+        },
+        "playback": {
+            "title": "Guardians vs. A's Highlights",
+            "description": "Josh Naylor and the Guardians take on JJ Bleday and the A's on March 31, 2024",
+            "mlbVideoUrl": "https://mlb-cuts-diamond.mlb.com/FORGE/2024/2024-03/31/ff1a9f79-4fd53e2f-ebf93195-csvm-diamondx64-asset_1280x720_59_4000K.mp4",
+            "mlbVideoDuration": "00:03:14",
+            "processedVideoUrl": null,
+            "processedVideoDuration": null,
+            "orientation": "HORIZONTAL"
+        },
+        "game": {
+            "homeTeamId": 133,
+            "awayTeamId": 114,
+            "date": "2024-03-31T20:07:00Z",
+            "status": "Final"
+        }
+    },
+    {
+        "id": "mJu4PMRFZrZjFsBQPxKXeY",
+        "gameId": 745279,
+        "createdAt": "2024-12-23T18:12:01.368Z",
+        "likes": 0,
+        "thumbnail": {
+            "main": null,
+            "fallback": "https://img.mlbstatic.com/mlb-images/image/upload/w_1920,h_1080,f_jpg,c_fill,g_auto/v1711923671/mlb/dz11fpfwojcukjg5fio2.jpg"
+        },
+        "playback": {
+            "title": "Red Sox vs. Mariners Highlights",
+            "description": "Tyler O'Neill and the Red Sox take on Mitch Haniger and the Mariners on March 31, 2024",
+            "mlbVideoUrl": "https://mlb-cuts-diamond.mlb.com/FORGE/2024/2024-03/31/8b23f762-f19bf53e-eb4e4c29-csvm-diamondx64-asset_1280x720_59_4000K.mp4",
+            "mlbVideoDuration": "00:03:00",
+            "processedVideoUrl": null,
+            "processedVideoDuration": null,
+            "orientation": "HORIZONTAL"
+        },
+        "game": {
+            "homeTeamId": 136,
+            "awayTeamId": 111,
+            "date": "2024-03-31T20:10:00Z",
+            "status": "Final"
+        }
+    },
+    {
+        "id": "ckweM7vgE2APVjfsesECj6",
+        "gameId": 747222,
+        "createdAt": "2024-12-23T18:12:00.352Z",
+        "likes": 0,
+        "thumbnail": {
+            "main": null,
+            "fallback": "https://img.mlbstatic.com/mlb-images/image/upload/w_1920,h_1080,f_jpg,c_fill,g_auto/mlb/qxmgfj2kpcjuzt3dvtgd.jpg"
+        },
+        "playback": {
+            "title": "Rockies vs. D-Backs Highlights",
+            "description": "Nolan Jones and the Rockies take on Corbin Carroll and the D-backs on March 31, 2024",
+            "mlbVideoUrl": "https://mlb-cuts-diamond.mlb.com/FORGE/2024/2024-03/31/1a925696-2ed17bb4-52d443a6-csvm-diamondx64-asset_1280x720_59_4000K.mp4",
+            "mlbVideoDuration": "00:03:14",
+            "processedVideoUrl": null,
+            "processedVideoDuration": null,
+            "orientation": "HORIZONTAL"
+        },
+        "game": {
+            "homeTeamId": 109,
+            "awayTeamId": 115,
+            "date": "2024-03-31T20:10:00Z",
+            "status": "Final"
+        }
+    },
+    {
+        "id": "eAzTscdioNcL6jD3aDdGQP",
+        "gameId": 746166,
+        "createdAt": "2024-12-23T18:12:03.252Z",
+        "likes": 0,
+        "thumbnail": {
+            "main": null,
+            "fallback": "https://img.mlbstatic.com/mlb-images/image/upload/w_1920,h_1080,f_jpg,c_fill,g_auto/mlb/gaebf8wjzvznl4tuh4zw.jpg"
+        },
+        "playback": {
+            "title": "Cardinals vs. Dodgers Highlights",
+            "description": "Paul Goldschmidt and the Cardinals take on Teoscar Hernández and the Dodgers on March 31, 2024",
+            "mlbVideoUrl": "https://mlb-cuts-diamond.mlb.com/FORGE/2024/2024-03/31/105c3e4d-1715550d-17808a92-csvm-diamondx64-asset_1280x720_59_4000K.mp4",
+            "mlbVideoDuration": "00:03:00",
+            "processedVideoUrl": null,
+            "processedVideoDuration": null,
+            "orientation": "HORIZONTAL"
+        },
+        "game": {
+            "homeTeamId": 119,
+            "awayTeamId": 138,
+            "date": "2024-03-31T23:10:00Z",
+            "status": "Final"
+        }
+    },
+    {
+        "id": "thdhh94kzxLTPc7Wmkpkja",
+        "gameId": 745035,
+        "createdAt": "2024-12-23T18:11:58.401Z",
+        "likes": 0,
+        "thumbnail": {
+            "main": null,
+            "fallback": "https://img.mlbstatic.com/mlb-images/image/upload/w_1920,h_1080,f_jpg,c_fill,g_auto/mlb/kvhltcsxzea7ler6jca4.jpg"
+        },
+        "playback": {
+            "title": "Cubs vs. Rangers Highlights",
+            "description": "Ian Happ and the Cubs take on Marcus Semien and the Rangers on March 31st, 2024",
+            "mlbVideoUrl": "https://mlb-cuts-diamond.mlb.com/FORGE/2024/2024-03/31/1c2e106d-e8047670-fcaf21c1-csvm-diamondx64-asset_1280x720_59_4000K.mp4",
+            "mlbVideoDuration": "00:03:10",
+            "processedVideoUrl": null,
+            "processedVideoDuration": null,
+            "orientation": "HORIZONTAL"
+        },
+        "game": {
+            "homeTeamId": 140,
+            "awayTeamId": 112,
+            "date": "2024-03-31T18:35:00Z",
+            "status": "Final"
+        }
+    },
+    {
+        "id": "eAzTscdioNcL6jD3aDdGQP",
+        "gameId": 746166,
+        "createdAt": "2024-12-23T18:12:03.252Z",
+        "likes": 0,
+        "thumbnail": {
+            "main": null,
+            "fallback": "https://img.mlbstatic.com/mlb-images/image/upload/w_1920,h_1080,f_jpg,c_fill,g_auto/mlb/gaebf8wjzvznl4tuh4zw.jpg"
+        },
+        "playback": {
+            "title": "Cardinals vs. Dodgers Highlights",
+            "description": "Paul Goldschmidt and the Cardinals take on Teoscar Hernández and the Dodgers on March 31, 2024",
+            "mlbVideoUrl": "https://mlb-cuts-diamond.mlb.com/FORGE/2024/2024-03/31/105c3e4d-1715550d-17808a92-csvm-diamondx64-asset_1280x720_59_4000K.mp4",
+            "mlbVideoDuration": "00:03:00",
+            "processedVideoUrl": null,
+            "processedVideoDuration": null,
+            "orientation": "HORIZONTAL"
+        },
+        "game": {
+            "homeTeamId": 119,
+            "awayTeamId": 138,
+            "date": "2024-03-31T23:10:00Z",
+            "status": "Final"
+        }
+    },
+    {
+        "id": "trC1sgsxxhNnkpw8mv8wdF",
+        "gameId": 745438,
+        "createdAt": "2024-12-23T18:12:02.328Z",
+        "likes": 0,
+        "thumbnail": {
+            "main": null,
+            "fallback": "https://img.mlbstatic.com/mlb-images/image/upload/w_1920,h_1080,f_jpg,c_fill,g_auto/mlb/xa8bjpapal90yqhmo4bk.jpg"
+        },
+        "playback": {
+            "title": "Giants vs. Padres Highlights",
+            "description": "Thairo Estrada and the Giants take on Ha-Seong Kim and the Padres on March 31, 2024",
+            "mlbVideoUrl": "https://mlb-cuts-diamond.mlb.com/FORGE/2024/2024-03/31/30ecefe0-4972ca9c-8b6c1545-csvm-diamondx64-asset_1280x720_59_4000K.mp4",
+            "mlbVideoDuration": "00:03:12",
+            "processedVideoUrl": null,
+            "processedVideoDuration": null,
+            "orientation": "HORIZONTAL"
+        },
+        "game": {
+            "homeTeamId": 135,
+            "awayTeamId": 137,
+            "date": "2024-03-31T20:10:00Z",
+            "status": "Final"
+        }
+    },
+    {
+        "id": "mJu4PMRFZrZjFsBQPxKXeY",
+        "gameId": 745279,
+        "createdAt": "2024-12-23T18:12:01.368Z",
+        "likes": 0,
+        "thumbnail": {
+            "main": null,
+            "fallback": "https://img.mlbstatic.com/mlb-images/image/upload/w_1920,h_1080,f_jpg,c_fill,g_auto/v1711923671/mlb/dz11fpfwojcukjg5fio2.jpg"
+        },
+        "playback": {
+            "title": "Red Sox vs. Mariners Highlights",
+            "description": "Tyler O'Neill and the Red Sox take on Mitch Haniger and the Mariners on March 31, 2024",
+            "mlbVideoUrl": "https://mlb-cuts-diamond.mlb.com/FORGE/2024/2024-03/31/8b23f762-f19bf53e-eb4e4c29-csvm-diamondx64-asset_1280x720_59_4000K.mp4",
+            "mlbVideoDuration": "00:03:00",
+            "processedVideoUrl": null,
+            "processedVideoDuration": null,
+            "orientation": "HORIZONTAL"
+        },
+        "game": {
+            "homeTeamId": 136,
+            "awayTeamId": 111,
+            "date": "2024-03-31T20:10:00Z",
+            "status": "Final"
+        }
+    },
+    {
+        "id": "trC1sgsxxhNnkpw8mv8wdF",
+        "gameId": 745438,
+        "createdAt": "2024-12-23T18:12:02.328Z",
+        "likes": 0,
+        "thumbnail": {
+            "main": null,
+            "fallback": "https://img.mlbstatic.com/mlb-images/image/upload/w_1920,h_1080,f_jpg,c_fill,g_auto/mlb/xa8bjpapal90yqhmo4bk.jpg"
+        },
+        "playback": {
+            "title": "Giants vs. Padres Highlights",
+            "description": "Thairo Estrada and the Giants take on Ha-Seong Kim and the Padres on March 31, 2024",
+            "mlbVideoUrl": "https://mlb-cuts-diamond.mlb.com/FORGE/2024/2024-03/31/30ecefe0-4972ca9c-8b6c1545-csvm-diamondx64-asset_1280x720_59_4000K.mp4",
+            "mlbVideoDuration": "00:03:12",
+            "processedVideoUrl": null,
+            "processedVideoDuration": null,
+            "orientation": "HORIZONTAL"
+        },
+        "game": {
+            "homeTeamId": 135,
+            "awayTeamId": 137,
+            "date": "2024-03-31T20:10:00Z",
+            "status": "Final"
+        }
+    }
+]
