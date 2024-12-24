c063f753d8fc1279c677ffd63c56f840c5e0c82b
benrobo <alumonabenaiah71@gmail.com>
2024-12-24 23:04:22 +0100

    added major updates

--- app/src/routes/home/feed/+page.svelte
+++ app/src/routes/home/feed/+page.svelte
@@ -2,18 +2,25 @@
 	import Flex from '@/components/Flex.svelte';
 	import { sampleHighlights } from '@/data/highlights';
 	import HighlightVideo from '@/modules/discover/components/highlight-video.svelte';
+	import EngagementBar from '@/modules/discover/components/EngagementBar.svelte';
 	import { onMount } from 'svelte';
 
 	const higlights = sampleHighlights;
 </script>
 
-<div class="fixed inset-0 flex items-center justify-center">
+<div class="w-full h-full flex items-center justify-center">
 	<div class="w-full h-full max-w-[600px] relative">
-		<div
-			class="w-full h-full highlight-videos-container overflow-y-auto snap-y snap-mandatory hideScrollBar3"
-		>
+		<div class="w-full h-full highlight-videos-container overflow-y-auto hideScrollBar2">
 			{#each higlights as hl}
-				<HighlightVideo {hl} />
+				<div class="w-full h-full snap-start snap-always">
+					<HighlightVideo
+						{hl}
+						last_video_id={higlights[higlights.length - 1]?.id}
+						onObServedDataId={(id) => {
+							// refetch new video based on the last observed id
+						}}
+					/>
+				</div>
 			{/each}
 		</div>
 	</div>
@@ -25,6 +32,12 @@
 	}
 
 	.highlight-videos-container {
-		/* scroll-padding-top: 20px; */
+		height: 100%;
+		scroll-snap-type: y mandatory;
+	}
+
+	.highlight-videos-container > div {
+		scroll-snap-align: start;
+		scroll-snap-stop: always;
 	}
 </style>
