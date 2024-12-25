9b496ba29d64a3ecce4bf414dfcbbe121aad73ca
benrobo <alumonabenaiah71@gmail.com>
2024-12-25 02:15:36 +0100

    added major updates

---
+++ app/src/lib/components/Portal.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { tick } from 'svelte';
+
+	let ref: HTMLElement;
+	const target = document.body;
+
+	onMount(() => {
+		target.appendChild(ref);
+		return () => {
+			target.removeChild(ref);
+		};
+	});
+</script>
+
+<div bind:this={ref}>
+	<slot />
+</div>
