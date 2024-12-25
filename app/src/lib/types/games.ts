2b5b8200fd856d68897edb4012937fbab2a1a6e5
benrobo <alumonabenaiah71@gmail.com>
2024-12-25 13:03:41 +0100

    added major updates

--- app/src/lib/types/games.ts
+++ app/src/lib/types/games.ts
@@ -4,3 +4,10 @@ export type SavePreference = {
     teams: number[];
     players?: number[];
 }
+
+export type MLBGlossary = {
+    id: string;
+    title: string;
+    url: string;
+    definition: string;
+}
