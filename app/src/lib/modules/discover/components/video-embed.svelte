2b8db715a476a2f97880b01456b6e43ec9f82b14
benrobo <alumonabenaiah71@gmail.com>
2024-12-25 18:18:49 +0100

    added major updates

---
+++ app/src/lib/modules/discover/components/video-embed.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	export let url: string = '';
+</script>
+
+<video
+	id="my-video"
+	class="video-js vjs-default-skin"
+	controls
+	preload="auto"
+	width="640"
+	height="360"
+>
+	<source src={url} type="video/mp4" />
+</video>
