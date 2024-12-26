9d8fd22e244a9605b143fe1d9c28ce8e5c9342df
benrobo <alumonabenaiah71@gmail.com>
2024-12-26 02:28:41 +0100

    added major updates

--- app/src/lib/http/requests.ts
+++ app/src/lib/http/requests.ts
@@ -20,3 +20,27 @@ export const hasPreference = async () => {
 	const res = await $axios.get('/user/has-preference');
 	return res.data;
 };
+
+export const getRecommendations = async (props: {
+	type?: "foryou" | "explore";
+	cursor?: string | null;
+	limit?: number;
+}) => {
+	let url = `/recommendations/feed`;
+	const urlParams = new URLSearchParams();
+
+	if(props.type) urlParams.append("type", props.type);
+	if(props.cursor) urlParams.append("cursor", props.cursor);
+	if(props.limit) urlParams.append("limit", String(props.limit));
+
+	if(urlParams.toString()) url += `?${urlParams.toString()}`
+
+	const res = await $axios.get(url);
+	return res.data;
+};
+
+export const markHighlightVideoAsSeen = async (playbackId: string) => {
+	let url = `/recommendations/highlight/mark-seen?playbackId=${playbackId}`;
+	const res = await $axios.post(url);
+	return res.data;
+};
