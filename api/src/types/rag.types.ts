78d30a1091de49d5ad3871ce8900c001aaeda072
benrobo <alumonabenaiah71@gmail.com>
2024-12-28 20:45:40 +0100

    added major updates

---
+++ api/src/types/rag.types.ts
@@ -0,0 +1,21 @@
+export type ValidRAGToolToolName = "search_web";
+
+export interface RAGTool {
+  name: ValidRAGToolToolName;
+  parameters: string[];
+  required_parameters: string[];
+  description: string;
+}
+
+export type SEARCH_WEB_RESPONSE = {
+  id: string;
+  url: string;
+  title: string;
+  author: string;
+  score: number;
+  summary: string;
+};
+
+export type CallToolResponse<T extends ValidRAGToolToolName> = {
+  search_web: SEARCH_WEB_RESPONSE[];
+}[T];
