3cd59eafd9c670297d859c1449fa56be2b7ec8f9
benrobo <alumonabenaiah71@gmail.com>
2024-12-28 02:37:32 +0100

    added major updates

---
+++ app/src/lib/modules/chat/components/AIResponse.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+	import Logo from '@/components/branding/Logo.svelte';
+	import LogoWithText from '@/components/branding/LogoWithText.svelte';
+	import Flex from '@/components/Flex.svelte';
+	import { fakeSources } from '@/data/chatfeed';
+	import type { ChatFeedSources } from '@/types/chatfeed';
+	import { Pickaxe } from 'lucide-svelte';
+
+	export let answer: string;
+	export let sources: ChatFeedSources[] = [];
+</script>
+
+<Flex className="w-full h-auto flex-col gap-4">
+	<!-- sources sections -->
+	<Flex className="w-full h-auto flex-col">
+		<Flex className="w-full h-auto flex-row items-center">
+			<Pickaxe size={18} class="stroke-white-200" />
+			<h1 class="text-white-200 font-recoleta font-normal text-lg">Sources</h1>
+		</Flex>
+		<div class="w-full h-auto flex flex-row items-start justify-start gap-3 overflow-x-auto mt-2">
+			{#each sources as src}
+				<!-- <Flex
+					className="w-full h-[100px] flex-row items-center justify-between bg-dark-106 p-2 rounded-md"
+				>
+					<Flex className="w-full h-auto flex-col">
+						<img
+							src={src.seo?.favicon}
+							class="w-[20px] h-[20px] rounded-full bg-white-100"
+							alt="favicon"
+						/>
+						<h1 class="text-white-100 font-recoleta font-normal text-sm">{src?.description}</h1>
+					</Flex>
+					<Flex className="w-auto h-auto flex-col items-end">
+						<img src={src?.image} class="w-[250px] rounded-md" alt="source" />
+					</Flex>
+				</Flex> -->
+				<a href={src?.url} target="_blank" class="w-auto">
+					<Flex
+						className="w-auto px-2 py-2 rounded-full bg-dark-106 border-[.5px] border-white-400/30 flex-center"
+					>
+						<img
+							src={src.seo?.favicon}
+							class="w-[17px] h-[17px] rounded-full bg-white-100"
+							alt="favicon"
+						/>
+						<h1 class="text-white-300 font-recoleta font-normal text-[12px]">
+							{src?.seo?.site_name}
+						</h1>
+					</Flex>
+				</a>
+			{/each}
+		</div>
+	</Flex>
+
+	<!-- answer -->
+	<Flex className="w-full h-auto flex-col mt-3">
+		<Flex className="w-full h-auto flex-row items-center">
+			<span class="p-1 rounded-full bg-dark-106">
+				<Logo size={18} className="stroke-red-302 text-red-302" />
+			</span>
+			<h1 class="text-white-200 font-recoleta font-normal text-lg">Answer</h1>
+		</Flex>
+
+		<!-- answer content -->
+		<div class="w-full h-auto">
+			<span class="text-white-200 font-recoleta font-normal text-md sm:text-lg">
+				{answer}
+			</span>
+		</div>
+	</Flex>
+</Flex>
---
+++ app/src/lib/modules/chat/components/ChatFeed.svelte
@@ -0,0 +1,116 @@
+<script lang="ts">
+	import Flex from '@/components/Flex.svelte';
+	import { ArrowRight, ChevronLeft, X } from 'lucide-svelte';
+	import { fly, slide } from 'svelte/transition';
+	import { quintOut } from 'svelte/easing';
+	import Portal from '@/components/Portal.svelte';
+	import { cn } from '@/utils';
+	import { onMount } from 'svelte';
+	import { authStore } from '@/store/auth.store';
+	import AiResponse from './AIResponse.svelte';
+	import { fakeMessages, fakeSources } from '@/data/chatfeed';
+
+	export let chatId: string | null;
+	export let onClose: () => void = () => {};
+
+	$: userAvatar = $authStore?.user?.avatar;
+
+	onMount(() => {});
+</script>
+
+{#if chatId}
+	<Portal>
+		<div
+			id="chat-feed"
+			class={cn(
+				'w-full max-w-[643px] mx-auto h-[98vh] bg-dark-107',
+				'transform transition-transform duration-500 ease-in-out fixed bottom-0 left-0 right-0 z-[999] rounded-t-[10px]',
+				'flex flex-col overflow-hidden drop-shadow-2xl'
+			)}
+		>
+			<div class="w-full mx-auto overflow-hidden">
+				<!-- topbar -->
+				<Flex className="w-full h-auto items-center justify-between px-4 pt-8 pb-3">
+					<button
+						class="p-[6px] bg-dark-106 rounded-full flex-center enableBounceEffect"
+						on:click={onClose}
+					>
+						<X size={20} class="stroke-white-100" strokeWidth={2} />
+					</button>
+					<div>
+						<h1 class="text-white-100 font-medium text-lg">Chat</h1>
+					</div>
+					<button>
+						<!-- <ChevronLeft size={24} class="stroke-white-100" /> -->
+					</button>
+				</Flex>
+
+				<!-- chat feed -->
+				<div class="w-full h-screen overflow-y-auto pb-[20em] px-5 py-3 hideScrollBar2">
+					{#each fakeMessages as msg}
+						<!-- ai response -->
+						{#if msg.role === 'ai'}
+							{@const sources = msg.sources?.map((idx) => fakeSources[idx]) ?? []}
+							<AiResponse answer={msg.content} {sources} />
+						{:else}
+							<!-- human response -->
+							<Flex className="w-full h-auto flex-col gap-3 my-10">
+								<Flex className="w-full h-auto flex-row items-center">
+									<img
+										src={$authStore?.user?.avatar}
+										class="w-[20px] h-[20px] rounded-full bg-white-100"
+										alt="favicon"
+										on:error={(e) => {
+											userAvatar = `https://api.dicebear.com/6.x/micah/svg?seed=${$authStore?.user?.email}`;
+										}}
+									/>
+									<div class="w-full h-auto">
+										<span class="text-white-200 font-poppins font-normal text-sm sm:text-lg">
+											{msg?.content}
+										</span>
+									</div>
+								</Flex>
+							</Flex>
+						{/if}
+					{/each}
+				</div>
+
+				<!-- bottom floating chatbox -->
+				<div
+					class="w-full min-h-[100px] px-8 absolute bottom-0 left-0 bg-dark-100/10 backdrop-blur-sm"
+				>
+					<Flex
+						className="w-full h-[50px] bg-dark-106 items-center justify-between pl-4 pr-1 rounded-full border-[1px] border-gray-101/50 shadow-md shadow-dark-100"
+					>
+						<input
+							type="text"
+							placeholder="Ask me anything about this highlight..."
+							class="w-auto h-full py-3 bg-transparent text-white-200 font-recoleta font-normal text-sm border-none outline-none ring-0 focus:border-none focus:ring-0 placeholder:text-white-200/70"
+						/>
+
+						<button class="w-[38px] h-[38px] bg-gray-101 rounded-full flex-center">
+							<ArrowRight />
+						</button>
+					</Flex>
+				</div>
+			</div>
+		</div>
+	</Portal>
+{/if}
+
+<style>
+	#chat-feed {
+		animation: slide-in-bottom 0.1s ease-in-out forwards;
+	}
+
+	@keyframes slide-in-bottom {
+		0% {
+			transform: translateY(1000px);
+			/* opacity: 0; */
+		}
+		100% {
+			transform: translateY(0);
+			/* opacity: 1; */
+		}
+	}
+</style>
