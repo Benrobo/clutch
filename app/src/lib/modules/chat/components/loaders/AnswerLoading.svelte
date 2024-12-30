c2999d4bbabae37c8558f72107255b36908487da
benrobo <alumonabenaiah71@gmail.com>
2024-12-30 12:14:14 +0100

    added major updates

---
+++ app/src/lib/modules/chat/components/loaders/AnswerLoading.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import Logo from '@/components/branding/Logo.svelte';
+	import Flex from '@/components/Flex.svelte';
+</script>
+
+<Flex className="w-full h-auto flex-col mt-7">
+	<Flex className="w-full h-auto flex-row items-center">
+		<span class="p-1 rounded-full bg-dark-106">
+			<Logo size={18} className="stroke-red-302 text-red-302" />
+		</span>
+		<h1 class="text-white-200 font-recoleta font-normal text-lg">Answer</h1>
+	</Flex>
+
+	<!-- answer content loader -->
+	<Flex className="w-full flex-col">
+		<div class="w-full max-w-[600px] h-[10px] rounded-full bg-gray-101/20 animate-pulse"></div>
+		<div class="w-full max-w-[450px] h-[10px] rounded-full bg-gray-101/20 animate-pulse"></div>
+		<div class="w-full max-w-[350px] h-[10px] rounded-full bg-gray-101/20 animate-pulse"></div>
+		<div class="w-full max-w-[250px] h-[10px] rounded-full bg-gray-101/20 animate-pulse"></div>
+	</Flex>
+</Flex>
---
+++ app/src/lib/modules/chat/components/loaders/ChatLoading.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import Flex from '@/components/Flex.svelte';
+	import useDetectDevice from '@/hooks/useDetectDevice';
+	import { afterUpdate } from 'svelte';
+	import AnswerLoading from './AnswerLoading.svelte';
+</script>
+
+<Flex className="w-full h-auto flex-col gap-4">
+	<!-- sources shimmer animation -->
+	<!-- desktop -->
+	<Flex
+		className="w-full h-auto hidden md:flex lg:flex flex-row items-center gap-3 overflow-hidden"
+	>
+		{#each Array(4) as _}
+			<div
+				class="w-full h-[36px] bg-dark-106 border-[.5px] border-white-400/30 flex flex-row items-center justify-center flex-nowrap gap-2 px-[5px] py-[4px] rounded-full animate-pulse"
+			>
+				<div class="min-w-[30px] h-full rounded-full bg-gray-101/20" />
+				<div class="w-full min-h-[10px] rounded-full bg-gray-101/20" />
+			</div>
+		{/each}
+	</Flex>
+
+	<!-- mobile -->
+	<Flex className="w-full h-auto md:hidden lg:hidden flex-row items-center gap-3 overflow-hidden">
+		{#each Array(3) as _}
+			<div
+				class="w-full h-[36px] bg-dark-106 border-[.5px] border-white-400/30 flex flex-row items-center justify-center flex-nowrap gap-2 px-[5px] py-[4px] rounded-full animate-pulse"
+			>
+				<div class="min-w-[30px] h-full rounded-full bg-gray-101/20" />
+				<div class="w-full min-h-[10px] rounded-full bg-gray-101/20" />
+			</div>
+		{/each}
+	</Flex>
+
+	<AnswerLoading />
+</Flex>
