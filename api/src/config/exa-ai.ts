74ac94fa48feef6ab1ef01518ece74bffcf30140
benrobo <alumonabenaiah71@gmail.com>
2024-12-30 05:06:12 +0100

    added major updates

--- api/src/config/exa-ai.ts
+++ api/src/config/exa-ai.ts
@@ -1,6 +1,6 @@
-import Exa from "exa-js";
+import { createExaClient } from "./exa-js-client.js";
 import env from "./env.js";
 
-const exa = new Exa(env.EXA_AI_API_KEY);
+const exa = createExaClient(env.EXA_AI_API_KEY);
 
 export default exa;
