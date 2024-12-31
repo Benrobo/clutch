c5c0464a25bbc33f80aef4d2aa0d90e9ca8772ff
benrobo <alumonabenaiah71@gmail.com>
2024-12-31 06:06:21 +0100

    added major updates

--- app/src/lib/modules/chat/components/AIResponse.svelte
+++ app/src/lib/modules/chat/components/AIResponse.svelte
@@ -9,16 +9,16 @@
 	export let sources: ChatFeedSources[] | null = [];
 </script>
 
-<Flex className="w-full h-auto flex-col gap-4 mt-3">
+<Flex className="w-full h-auto flex-col gap-4">
 	<!-- sources sections -->
-	<Flex className="w-full h-auto flex-col mt-8">
+	<Flex className="w-full h-auto flex-col">
 		{#if sources && sources.length > 0}
 			<Flex className="w-full h-auto flex-row items-center">
 				<Pickaxe size={18} class="stroke-white-200" />
 				<h1 class="text-white-200 font-recoleta font-normal text-md">Sources</h1>
 			</Flex>
 			<div
-				class="w-fit h-auto flex flex-row items-start justify-start gap-3 overflow-x-hidden mt-2 hideScrollBar2 whitespace-nowrap"
+				class="w-fit h-auto flex flex-row items-start justify-start gap-3 overflow-x-hidden mt-2 hideScrollBar2 whitespace-nowrap relative"
 			>
 				{#each sources as src}
 					<!-- <Flex
@@ -34,9 +34,9 @@
 					</Flex>
 					<Flex className="w-auto h-auto flex-col items-end">
 						<img src={src?.image} class="w-[250px] rounded-md" alt="source" />
-					</Flex>
+				</Flex>
 				</Flex> -->
-					<a href={src?.url} target="_blank" class="w-auto">
+					<a href={src?.url} target="_blank" class="w-auto whitespace-nowrap">
 						<Flex
 							className="w-auto px-2 py-2 rounded-full bg-dark-106 border-[.5px] border-white-400/30 flex-center"
 						>
@@ -66,7 +66,7 @@
 
 		<!-- answer content -->
 		<div class="w-full h-auto">
-			<span class="text-white-100 font-inter font-normal text-md sm:text-lg">
+			<span class="text-white-100 font-montserrat font-normal text-md sm:text-lg">
 				{answer}
 			</span>
 		</div>
