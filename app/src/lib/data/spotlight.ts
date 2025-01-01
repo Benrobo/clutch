7cb1cdc6b4d644e080bdf00cea10e82c52ea9bf8
benrobo <alumonabenaiah71@gmail.com>
2025-01-01 21:37:53 +0100

    added major updates

---
+++ app/src/lib/data/spotlight.ts
@@ -0,0 +1,46 @@
+
+
+export const fakeSpotlightData = {
+    "publishedDate": null,
+    "readingTime": {
+        "min": 3,
+        "sec": 239
+    },
+    "id": "cm51ctauo000u9rq81558yvmu",
+    "title": "Phillies use late rally to beat Braves for first win",
+    "headline": "Out? Safe? Phils rally past Braves after late-game confusion",
+    "body": "PHILADELPHIA -- To modify the old saying from Yogi Berra: It ain’t over ‘til it’s over -- even if most people in the stadium think it is literally over. The Phillies used a late rally to defeat the Braves, [5-4](https://www.mlb.com/gameday/745602/final/box), before a sellout crowd of 42,515 on a chilly holiday afternoon Sunday at Citizens Bank Park. It was Philadelphia's first win of the season -- and it almost didn’t happen.\n\nThe seventh inning was seemingly over after Johan Rojas grounded into what was ruled a double play on the field. After video review, crew chief Mark Wegner announced that the runner at second was indeed out. What he said next -- the call at first was overturned and Rojas was safe -- was drowned out by boos from the Philadelphia crowd reacting to the first part of the announcement. The Braves ran off the field, still holding a 3-2 lead. There was only one problem -- the inning was not over. Atlanta's players and manager Brian Snitker -- and most of the people in the stadium -- didn’t hear the umpire’s actual decision over the roar of displeasure from the crowd.\n\nAnd just like that, the Phillies had new life. The Braves returned to the field. The crowd grew even louder as Kyle Schwarber's walk-up song, “Cult of Personality” by Living Colour, blared over the speakers. He promptly singled to center, then Trea Turner ripped a single through the right side to score Rojas and tie the game at 3-3.\n\nTurner then stole second and Alec Bohm lined a single to left, scoring both Schwarber and Turner to give Philadelphia a 5-3 lead. The Phillies, who all afternoon looked like they would go out like lambs, instead ended March like lions.\n\nAnd it all started with a hustle play from Rojas, the light-hitting No. 9 hitter. “That’s the play of the game,” said Schwarber, who started the game with the 32nd leadoff home run -- a 114.4 mph shot off Chris Sale -- of his career. “Those little controllable things are what win baseball games.”\n\nRojas is known for his fielding and his baserunning -- the little things that can add up to big moments, which is exactly how Bryce Harper described it after the game. “That’s a big moment,” said Harper, who received a scheduled day off and is expected to return to the starting lineup on Monday. “It extended the inning; that’s the way you play up here.”\n\nIt provided a positive coda on what had been a rough series for Rojas, who is hitless in eight at-bats to begin the season. “I’ve been there,” said Bohm. “Everyone’s been there. It’s a hard game. He does a tremendous job for us. The hits are going to come. He’s put together some decent at-bats, and I think it’s only going to get better. The more at-bats he gets, the more comfortable he’ll become.” Thanks to Rojas, the Phillies were able to rally against Atlanta’s newly acquired lefty specialist Aaron Bummer. The hit from Schwarber was especially impressive, considering the lefty-on-lefty matchup against a reliever who was brought in to neutralize guys like Schwarber and the Phillies' other mighty lefty hitters. “You could say that,” Bohm said. “Maybe, maybe not. You see them acquiring lefties, the writing is on the wall. That’s how the league is going -- lefty specialists. These bullpens are getting nastier and nastier.”\n\nOf course, the game isn’t over until it’s over. This was the reigning division champion Braves, and the Phillies still needed six outs from their beleaguered bullpen. Atlanta narrowed the deficit to one with Ozzie Albies' RBI single off Seranthony Domínguez in the eighth. Gregory Soto took over and stranded runners at the corners to preserve the lead. José Alvarado, who had a rocky Opening Day performance, redeemed himself by shutting the door in the ninth to earn his first save of the season. “He’s as good as it gets coming out of the bullpen when the cutter has that much velocity and that much depth,” said manager Rob Thomson.\n\nWith their first win in the books, the Phillies will now welcome the Reds to town on Monday -- and they'll do so with Harper in the lineup. “I’m definitely looking forward to getting in there tomorrow,” Harper said. He’ll be suiting up for a team that has a very different feel from Saturday, when Harper’s health was unknown and the Phillies were reeling from two straight losses. But these NL East rivals know all too well that it's how you finish that matters. The Braves have won six straight division titles, but the Phillies knocked them out of the postseason in each of the past two years. On Sunday, the Phillies spoiled the ending once again.\n",
+    "photo": "https://img.mlbstatic.com/mlb-images/image/upload/w_1920,h_1080,f_jpg,c_fill,g_auto/mlb/gyotcxek5gduoxfb9ggj.jpg",
+    "highlight": {
+        "id": "ho1PaFWiUsfok4iaPaennH",
+        "game_id": 745602
+    },
+    "sources": [
+        {
+            "id": "cm51ctauo000u9rq81558yvmu",
+            "index": 0,
+            "sources": [
+                {
+                    "url": "https://www.mlb.com/gameday/745602/final/box",
+                    "title": null,
+                    "favicon": "https://www.mlbstatic.com/mlb.com/images/logos/apple-touch-icons-180x180/mlb.png"
+                },
+                {
+                    "url": "https://www.mlb.com/gameday/braves-vs-phillies/2024/03/31/745602/final/box",
+                    "title": "Braves 4, Phillies 5 Final Score (03/31/2024)",
+                    "favicon": "https://www.mlbstatic.com/mlb.com/images/logos/apple-touch-icons-180x180/mlb.png"
+                },
+                {
+                    "url": "https://www.mlb.com/schedule/gameday",
+                    "title": "MLB Gameday: Real-time MLB scores for your favorite teams",
+                    "favicon": "https://www.mlbstatic.com/team-logos/apple-touch-icons-180x180/mlb.png"
+                },
+                {
+                    "url": "https://www.mlb.com/es/gameday/braves-vs-phillies/2024/03/31/745602/final/box",
+                    "title": "Braves 4, Phillies 5 Resultado Final (31/03/2024)",
+                    "favicon": "https://www.mlbstatic.com/mlb.com/images/logos/apple-touch-icons-180x180/mlb.png"
+                }
+            ]
+        }
+    ]
+}
