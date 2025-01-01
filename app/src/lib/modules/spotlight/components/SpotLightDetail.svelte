2a5b82d6eb110e81ccfeaa034fa97b3fe9996817
benrobo <alumonabenaiah71@gmail.com>
2025-01-01 21:54:00 +0100

    added major updates

--- app/src/lib/modules/spotlight/components/SpotLightDetail.svelte
+++ app/src/lib/modules/spotlight/components/SpotLightDetail.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
 	import Flex from '@/components/Flex.svelte';
 	import { fakeSpotlightData } from '@/data/spotlight';
-	import { Clock } from 'lucide-svelte';
+	import { Clock, Maximize } from 'lucide-svelte';
 	import SpotlightSources from './SpotlightSources.svelte';
 	import MarkdownRenderer from '@/components/MarkdownRenderer.svelte';
-	import { capitalizeFirstLetter } from '@/utils';
+	import { capitalizeFirstLetter, cn } from '@/utils';
 
 	export let selectedSpotLight: string | null = null;
 
 	const spotlightData = fakeSpotlightData;
 	const content = spotlightData?.body?.split('\n\n').filter((c) => c?.length > 0);
+
+	$: shouldExpand = false;
 </script>
 
 <Flex
@@ -18,7 +20,7 @@
 	<!-- thumbnail -->
 	<img
 		src={fakeSpotlightData?.photo}
-		class="w-full h-[300px] object-cover bg-dark-100"
+		class="w-full h-[400px] object-cover bg-dark-100"
 		alt="spotlight"
 	/>
 
@@ -43,12 +45,36 @@
 	</Flex>
 
 	<!-- content -->
-	<Flex className="w-full h-auto flex flex-col gap-4 px-4 py-2">
+	<Flex
+		className={cn(
+			'w-full flex flex-col gap-4 px-4 py-2 relative',
+			content.join('\n\n').length > 300 && !shouldExpand ? 'h-[40vh] overflow-hidden' : 'h-auto'
+		)}
+	>
 		{#if content}
 			{#each content as c, index}
 				<MarkdownRenderer content={c} contentClass="font-jetbrains text-[13px]" />
 				<SpotlightSources {index} sources={fakeSpotlightData?.sources} />
 			{/each}
+		{/if}
+
+		{#if content.join('\n\n').length > 300}
+			<div
+				class={cn(
+					'w-full h-auto absolute bottom-0 z-[10] bg-gradient-to-b from-dark-106/10 to-dark-107 pt-10 pb-4 flex-center enableBounceEffect',
+					shouldExpand ? '-bottom-10 translate-y-5' : 'bottom-0'
+				)}
+			>
+				<button
+					class="w-auto px-4 py-2 rounded-full flex-center text-center text-white-200 bg-dark-106 gap-3 border-[1px] border-white-300/20"
+					on:click={() => {
+						shouldExpand = !shouldExpand;
+					}}
+				>
+					<Maximize size={15} class="stroke-white-200" />
+					<p class="text-xs font-poppins">{shouldExpand ? 'See Less' : 'See More'}</p>
+				</button>
+			</div>
 		{/if}
 	</Flex>
 </Flex>
