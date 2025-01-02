f40872fe8988be967cb36fd930ceff2dbfc9763a
benrobo <alumonabenaiah71@gmail.com>
2025-01-02 09:29:36 +0100

    added major updates

--- app/src/lib/components/BottomNavTab.svelte
+++ app/src/lib/components/BottomNavTab.svelte
@@ -9,6 +9,9 @@
 	import { afterUpdate, onMount } from 'svelte';
 	import { derived } from 'svelte/store';
 	import type { User } from '@/types/auth';
+	import { useGlobalStore } from '@/store/global.store';
+	import { fly } from 'svelte/transition';
+	import { quintOut } from 'svelte/easing';
 
 	$: pathname = derived(page, ($page) => $page.url.pathname);
 	$: slug = $pathname.split('/');
@@ -16,6 +19,8 @@
 	$: activeTab = slug[slug.length - 1] ?? 'discover';
 
 	$: userDetails = $authStore?.user as User;
+
+	$: globalStore = useGlobalStore();
 
 	const tabs = [
 		{
@@ -45,69 +50,82 @@
 		}
 	];
 
+	$: {
+		if (activeTab === 'dugout') {
+			globalStore.toggleBottomNav(false);
+		} else {
+			globalStore.toggleBottomNav(true);
+		}
+	}
+
 	afterUpdate(() => {});
 </script>
 
-<nav class="fixed bottom-0 left-0 right-0 z-[1] bg-dark-103 pb-safe">
-	<div class="max-w-[678px] mx-auto">
-		<Flex className="w-full py-3 border-t-[1px] border-t-gray-100">
-			{#each tabs as tab}
-				<button
-					class={cn(
-						'w-full transition-all flex flex-col items-center justify-center gap-1 text-white-100 enableBounceEffect',
-						activeTab === tab.id && 'text-red-302'
-					)}
-					on:click={() => {
-						goto(tab.href);
-					}}
-				>
-					{#if tab.id === 'feed'}
-						<GalleryHorizontal
-							size={20}
-							class={cn('stroke-white-100', activeTab === tab.id && 'stroke-red-302')}
-						/>
-					{:else if tab.id === 'spotlight'}
-						<Lightbulb
-							size={20}
-							class={cn('stroke-white-100', activeTab === tab.id && 'stroke-red-302')}
-						/>
-					{:else if tab.id === 'activities'}
-						<BellRing
-							size={20}
-							class={cn('stroke-white-100', activeTab === tab.id && 'stroke-red-302')}
-						/>
-					{:else if tab.id === 'dugout'}
-						<Gamepad_2
-							size={20}
-							class={cn('stroke-white-100', activeTab === tab.id && 'stroke-red-302')}
-						/>
-					{:else if tab.id === 'profile'}
-						<img
-							src={userDetails?.avatar}
-							alt=""
-							class="w-[22px] h-[22px] rounded-full"
-							on:error={(e) => {
-								// @ts-expect-error
-								e.currentTarget.src = '/baseball.png';
-							}}
-						/>
-					{:else}
-						{null}
-					{/if}
+{#if $globalStore?.hideBottomNav}
+	<nav
+		class={cn('fixed bottom-0 left-0 right-0 z-[1] bg-dark-103 pb-safe')}
+		transition:fly={{ y: 500, duration: 500, easing: quintOut }}
+	>
+		<div class="max-w-[678px] mx-auto">
+			<Flex className="w-full py-3 border-t-[1px] border-t-gray-100">
+				{#each tabs as tab}
+					<button
+						class={cn(
+							'w-full transition-all flex flex-col items-center justify-center gap-1 text-white-100 enableBounceEffect',
+							activeTab === tab.id && 'text-red-302'
+						)}
+						on:click={() => {
+							goto(tab.href);
+						}}
+					>
+						{#if tab.id === 'feed'}
+							<GalleryHorizontal
+								size={20}
+								class={cn('stroke-white-100', activeTab === tab.id && 'stroke-red-302')}
+							/>
+						{:else if tab.id === 'spotlight'}
+							<Lightbulb
+								size={20}
+								class={cn('stroke-white-100', activeTab === tab.id && 'stroke-red-302')}
+							/>
+						{:else if tab.id === 'activities'}
+							<BellRing
+								size={20}
+								class={cn('stroke-white-100', activeTab === tab.id && 'stroke-red-302')}
+							/>
+						{:else if tab.id === 'dugout'}
+							<Gamepad_2
+								size={20}
+								class={cn('stroke-white-100', activeTab === tab.id && 'stroke-red-302')}
+							/>
+						{:else if tab.id === 'profile'}
+							<img
+								src={userDetails?.avatar}
+								alt=""
+								class="w-[22px] h-[22px] rounded-full"
+								on:error={(e) => {
+									// @ts-expect-error
+									e.currentTarget.src = '/baseball.png';
+								}}
+							/>
+						{:else}
+							{null}
+						{/if}
 
-					{#if tab.title}
-						<span
-							class={cn(
-								'font-montserrat text-xs font-normal',
-								activeTab === tab.id && 'text-red-302'
-							)}>{tab.title}</span
-						>
-					{/if}
-				</button>
-			{/each}
-		</Flex>
-	</div>
-</nav>
+						{#if tab.title}
+							<span
+								class={cn(
+									'font-montserrat text-xs font-normal',
+									activeTab === tab.id && 'text-red-302'
+								)}>{tab.title}</span
+							>
+						{/if}
+					</button>
+				{/each}
+			</Flex>
+		</div>
+	</nav>
+{/if}
 
 <style>
 	nav {
