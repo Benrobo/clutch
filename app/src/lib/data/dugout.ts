554dae030c6b08cadf675b70a429757d2c1e1613
benrobo <alumonabenaiah71@gmail.com>
2025-01-02 09:29:37 +0100

    added major updates

---
+++ app/src/lib/data/dugout.ts
@@ -0,0 +1,16 @@
+
+
+export const DugoutGames = [
+    {
+        id: "word-search",
+        title: "Word Search",
+        description: "A word search game",
+        image: "/images/word-search.png",
+    },
+    {
+        id: "quizzes",
+        title: "Quizzes",
+        description: "",
+        image: "/images/word-search.png",
+    },
+]
