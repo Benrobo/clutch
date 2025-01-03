6379b722d2195da2bb59bc21965db8000f5a3273
benrobo <alumonabenaiah71@gmail.com>
2025-01-03 08:50:55 +0100

    added major updates

---
+++ app/src/routes/home/dugout/[id]/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { quintOut } from 'svelte/easing';
+	import { fade, fly } from 'svelte/transition';
+	import WordSearch from '$lib/modules/dugout/components/WordSearch/index.svelte';
+	import Quiz from '$lib/modules/dugout/components/Quiz/index.svelte';
+	import FourPicOneWord from '$lib/modules/dugout/components/FourPicOneWord/index.svelte';
+
+	const slug = $page.params.id;
+</script>
+
+<div class="w-screen h-screen" transition:fade={{ duration: 500, easing: quintOut }}>
+	{#if slug === 'word-search'}
+		<WordSearch />
+	{:else if slug === 'quiz'}
+		<Quiz />
+	{:else if slug === '4-pic-one-word'}
+		<FourPicOneWord />
+	{/if}
+</div>
