c5f3cb4dd17808ca6ba0a4dbbad6366436c3e701
benrobo <alumonabenaiah71@gmail.com>
2025-01-04 03:02:54 +0100

    added major updates

---
+++ app/src/lib/modules/dugout/components/FourPicOneWord/TileDisplay.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let secretWord: string = '';
+</script>
+
+<div class="w-full min-h-[150px] flex flex-col items-center justify-center">
+	<button
+		class="w-[45px] h-[45px] border-[2px] bg-[#252240] border-[#5D5081] rounded-lg overflow-hidden"
+	>
+		<span class="text-white text-2xl font-bold">A</span>
+	</button>
+</div>
