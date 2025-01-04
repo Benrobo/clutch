fd383db8b57bdd55e572e421046803e68bd2a7ed
benrobo <alumonabenaiah71@gmail.com>
2025-01-04 14:44:30 +0100

    added major updates

--- app/src/lib/modules/dugout/components/ThreeDButton.svelte
+++ app/src/lib/modules/dugout/components/ThreeDButton.svelte
@@ -3,7 +3,7 @@
 	import type { ClassValue } from 'tailwind-variants';
 
 	export let className: ClassValue = '';
-	export let colorType: 'orange' | 'blue' | 'pink' = 'orange';
+	export let colorType: 'orange' | 'blue' | 'pink' | 'hotpink' = 'orange';
 	export let onClick: () => void = () => {};
 	export let disabled: boolean = false;
 </script>
@@ -17,6 +17,8 @@
 			'bg-gradient-to-b from-[#6FA3F9] via-[#5D85EF] to-[#4F65E4] border-t-[3px] border-t-[#9BE4FF] border-b-[6px] border-b-[#3B47DC] outline-2 outline outline-[#4F65E4]/90 active:brightness-90 active:translate-y-[2px] active:border-b-[3px] transition-all',
 		colorType === 'pink' &&
 			'bg-gradient-to-b from-[#FD7FA5] to-[#E54376] border-t-[#FFD4FF] border-t-[2px] active:brightness-90 active:translate-y-[1px] active:border-t-[2px] transition-all drop-shadow-md shadow-dark-100',
+		colorType === 'hotpink' &&
+			'bg-gradient-to-b from-[#D61B44] to-[#DA205D] border-t-[#FFD4FF] border-t-[2px] active:brightness-90 active:translate-y-[1px] active:border-t-[2px] transition-all drop-shadow-md shadow-dark-100',
 		className
 	)}
 	on:click={onClick}
