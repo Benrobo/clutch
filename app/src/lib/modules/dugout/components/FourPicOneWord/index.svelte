80246be6af9339ab1b147d55a80c4bcc43fdfc56
benrobo <alumonabenaiah71@gmail.com>
2025-01-04 03:02:53 +0100

    added major updates

--- app/src/lib/modules/dugout/components/FourPicOneWord/index.svelte
+++ app/src/lib/modules/dugout/components/FourPicOneWord/index.svelte
@@ -1,7 +1,43 @@
 <script lang="ts">
 	import Flex from '@/components/Flex.svelte';
+	import SplashScreen from './SplashScreen.svelte';
+	import GameArea from './GameArea.svelte';
+	import { fade } from 'svelte/transition';
+	import { cn } from '@/utils';
+
+	$: hideSplashScreen = true;
+	$: showGameArea = false;
+
+	const onHideSplashScreen = () => {
+		hideSplashScreen = !hideSplashScreen;
+	};
+
+	$: {
+		if (!hideSplashScreen) {
+			setTimeout(() => {
+				showGameArea = true;
+			}, 500);
+		}
+	}
 </script>
 
-<Flex className="w-full h-full max-w-[678px] mx-auto flex-center ">
-	<h1>4 Pic 1 Word</h1>
-</Flex>
+<div
+	class={cn(
+		'w-full h-full flex flex-col items-center justify-center max-w-[678px] mx-auto transition-all duration-1000 relative ',
+		!hideSplashScreen && 'bg-none duration-500'
+	)}
+	transition:fade={{ duration: 1000 }}
+>
+	{#if hideSplashScreen}
+		<div class="w-full h-full" transition:fade={{ duration: 500 }}>
+			<SplashScreen {onHideSplashScreen} />
+		</div>
+	{/if}
+
+	{#if showGameArea}
+		<GameArea />
+	{/if}
+</div>
+
+<style>
+</style>
