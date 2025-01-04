6ad15bdeb4152567849a3f475cd87c8051f5c3bd
benrobo <alumonabenaiah71@gmail.com>
2025-01-04 14:44:30 +0100

    added major updates

--- app/src/lib/modules/dugout/components/FourPicOneWord/GameArea.svelte
+++ app/src/lib/modules/dugout/components/FourPicOneWord/GameArea.svelte
@@ -2,32 +2,34 @@
 	import Flex from '@/components/Flex.svelte';
 	import { cn } from '@/utils';
 	import ThreeDButton from '../ThreeDButton.svelte';
-	import { Star } from 'lucide-svelte';
+	import { Star, X } from 'lucide-svelte';
 	import TileDisplay from './GameControl.svelte';
 	import GameControl from './GameControl.svelte';
 
 	const fakeMedia = [
 		{
 			type: 'image',
-			url: '/4-pic-one-word/batter.jpg',
+			url: '/4-pic-1-word/batter.jpg',
 			description: 'A batter'
 		},
 		{
 			type: 'image',
-			url: '/4-pic-one-word/baseball.jpeg',
+			url: '/4-pic-1-word/baseball.jpeg',
 			description: 'A baseball'
 		},
 		{
 			type: 'image',
-			url: '/4-pic-one-word/pitcher.jpg',
+			url: '/4-pic-1-word/pitcher.jpg',
 			description: 'A pitcher'
 		},
 		{
 			type: 'image',
-			url: '/4-pic-one-word/plate.jpeg',
+			url: '/4-pic-1-word/plate.jpeg',
 			description: 'Home plate'
 		}
 	];
+
+	export let leaveGame: () => void = () => {};
 </script>
 
 <div
@@ -40,23 +42,33 @@
 		class="w-full h-auto min-h-[100px] flex justify-end items-end bg-gradient-to-b from-[#5B3A76] from-83% to-[#533971] border-b-[10px] border-b-[#3F2F5D]"
 	>
 		<Flex className="w-full h-full items-center justify-between px-5">
-			<ThreeDButton colorType="pink" className="w-[45px] h-[45px] flex-center rounded-full">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="30"
-					height="30"
-					viewBox="0 0 24 24"
-					class="-translate-y-1"
-					><path
-						fill="none"
-						stroke="currentColor"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="1.5"
-						d="M15 21H9v-8.4a.6.6 0 0 1 .6-.6h4.8a.6.6 0 0 1 .6.6zm5.4 0H15v-2.9a.6.6 0 0 1 .6-.6h4.8a.6.6 0 0 1 .6.6v2.3a.6.6 0 0 1-.6.6M9 21v-4.9a.6.6 0 0 0-.6-.6H3.6a.6.6 0 0 0-.6.6v4.3a.6.6 0 0 0 .6.6zm1.806-15.887l.909-1.927a.312.312 0 0 1 .57 0l.91 1.927l2.032.311c.261.04.365.376.176.568l-1.47 1.5l.347 2.118c.044.272-.228.48-.462.351l-1.818-1l-1.818 1c-.233.128-.506-.079-.462-.351l.347-2.118l-1.47-1.5c-.19-.192-.085-.528.175-.568z"
-					/></svg
+			<Flex className="w-auto h-auto gap-2 items-center">
+				<ThreeDButton
+					colorType="hotpink"
+					className="w-[45px] h-[45px] flex-center rounded-full"
+					onClick={leaveGame}
 				>
-			</ThreeDButton>
+					<X />
+				</ThreeDButton>
+
+				<ThreeDButton colorType="pink" className="w-[45px] h-[45px] flex-center rounded-full">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="30"
+						height="30"
+						viewBox="0 0 24 24"
+						class="-translate-y-1"
+						><path
+							fill="none"
+							stroke="currentColor"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="1.5"
+							d="M15 21H9v-8.4a.6.6 0 0 1 .6-.6h4.8a.6.6 0 0 1 .6.6zm5.4 0H15v-2.9a.6.6 0 0 1 .6-.6h4.8a.6.6 0 0 1 .6.6v2.3a.6.6 0 0 1-.6.6M9 21v-4.9a.6.6 0 0 0-.6-.6H3.6a.6.6 0 0 0-.6.6v4.3a.6.6 0 0 0 .6.6zm1.806-15.887l.909-1.927a.312.312 0 0 1 .57 0l.91 1.927l2.032.311c.261.04.365.376.176.568l-1.47 1.5l.347 2.118c.044.272-.228.48-.462.351l-1.818-1l-1.818 1c-.233.128-.506-.079-.462-.351l.347-2.118l-1.47-1.5c-.19-.192-.085-.528.175-.568z"
+						/></svg
+					>
+				</ThreeDButton>
+			</Flex>
 
 			<!-- level -->
 			<div class="w-auto h-auto text-white text-2xl font-poppins font-semibold">Level 1</div>
