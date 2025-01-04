1bc6543eeea2bb9824ef20e6016afe6319ed2f06
benrobo <alumonabenaiah71@gmail.com>
2025-01-04 15:38:09 +0100

    added major updates

--- api/src/controllers/dugout.controller.ts
+++ api/src/controllers/dugout.controller.ts
@@ -3,7 +3,11 @@ import DugoutService from "../services/dugout.service.js";
 import sendResponse from "../lib/send-response.js";
 import { HttpException } from "../lib/exception.js";
 import shortUUID from "short-uuid";
-import { VALID_GAME_IDS } from "../constant/dugout.js";
+import { GAME_LEVELS, VALID_GAME_IDS } from "../constant/dugout.js";
+import {
+  GameLevel,
+  VALID_GAME_IDS as VALID_GAME_IDS_TYPE,
+} from "../types/dugout.types.js";
 
 class DugoutController {
   private dugoutService: DugoutService;
@@ -38,7 +42,7 @@ class DugoutController {
     }
     const { gameId } = c.req.param();
 
-    if (!VALID_GAME_IDS.includes(gameId)) {
+    if (!VALID_GAME_IDS.includes(gameId as VALID_GAME_IDS_TYPE)) {
       throw new HttpException("Invalid game id", 400);
     }
 
@@ -50,6 +54,32 @@ class DugoutController {
     }
 
     return sendResponse.success(c, "Game joined successfully", 200, game);
+  }
+
+  async upgradeLevel(c: Context) {
+    const user = c.get("user");
+    const { gameId } = c.req.param();
+
+    if (!user?.id) {
+      throw new HttpException("Unauthorized", 401);
+    }
+
+    if (!VALID_GAME_IDS.includes(gameId as VALID_GAME_IDS_TYPE)) {
+      throw new HttpException("Invalid game id", 400);
+    }
+
+    const result = await this.dugoutService.upgradeLevel(gameId, user.id);
+
+    if (result.error) {
+      throw new HttpException(result.error, 400);
+    }
+
+    return sendResponse.success(
+      c,
+      "Level upgraded successfully",
+      200,
+      result?.success
+    );
   }
 }
 
