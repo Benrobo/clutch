550055ff36eb0740cdb6c449f488faa570954864
benrobo <alumonabenaiah71@gmail.com>
2025-01-04 15:38:09 +0100

    added major updates

--- api/src/constant/dugout.ts
+++ api/src/constant/dugout.ts
@@ -1,1 +1,28 @@
-export const VALID_GAME_IDS = ["4-pic-1-word", "quiz"];
+import {
+  GameProgressionChallenges,
+  VALID_GAME_IDS as VALID_GAME_IDS_TYPE,
+  GameLevel,
+} from "../types/dugout.types.js";
+import {
+  apprentice,
+  planetary,
+  stellar,
+} from "../data/games/4-pic-1-word/index.js";
+
+export const VALID_GAME_IDS: VALID_GAME_IDS_TYPE[] = ["4-pic-1-word", "quiz"];
+
+export const GAME_PROGRESSION_CHALLENGES = {
+  "4-pic-1-word": {
+    apprentice: apprentice.length,
+    planetary: planetary.length,
+    stellar: stellar.length,
+  },
+} satisfies GameProgressionChallenges;
+
+export const GAME_LEVELS = [
+  "apprentice",
+  "planetary",
+  "stellar",
+  "universe",
+  "domain_master",
+] as const;
