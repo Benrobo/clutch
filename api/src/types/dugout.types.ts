af40fbc3fbaf3d5ce65ce08e6069e56f1c982fe4
benrobo <alumonabenaiah71@gmail.com>
2025-01-04 16:47:10 +0100

    added major updates

--- api/src/types/dugout.types.ts
+++ api/src/types/dugout.types.ts
@@ -2,11 +2,37 @@ export type VALID_GAME_IDS = "4-pic-1-word" | "quiz";
 
 export type GameLevel = "apprentice" | "planetary" | "stellar";
 
+export type FourPicOneWordChallenges = {
+  id: number;
+  secret: {
+    word: string;
+    display: string;
+  };
+  definition: string;
+  media: Array<{
+    type: "image" | "video";
+    url: string;
+    description: string;
+  }>;
+};
+
 export type GameProgressionChallenges = {
   [key in VALID_GAME_IDS]?: {
-    apprentice: number;
-    planetary: number;
-    stellar: number;
+    apprentice: {
+      count: number;
+      challenges: FourPicOneWordChallenges[];
+      points: number;
+    };
+    planetary: {
+      count: number;
+      challenges: FourPicOneWordChallenges[];
+      points: number;
+    };
+    stellar: {
+      count: number;
+      challenges: FourPicOneWordChallenges[];
+      points: number;
+    };
   };
 };
 
