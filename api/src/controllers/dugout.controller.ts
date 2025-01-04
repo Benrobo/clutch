be065daa993084318906671f0608f43e5b281e8f
benrobo <alumonabenaiah71@gmail.com>
2025-01-04 16:47:09 +0100

    added major updates

--- api/src/controllers/dugout.controller.ts
+++ api/src/controllers/dugout.controller.ts
@@ -5,6 +5,7 @@ import { HttpException } from "../lib/exception.js";
 import shortUUID from "short-uuid";
 import { GAME_LEVELS, VALID_GAME_IDS } from "../constant/dugout.js";
 import {
+  CompletedChallenges,
   GameLevel,
   VALID_GAME_IDS as VALID_GAME_IDS_TYPE,
 } from "../types/dugout.types.js";
@@ -79,6 +80,93 @@ class DugoutController {
       "Level upgraded successfully",
       200,
       result?.success
+    );
+  }
+
+  async getGameLevelChallenges(c: Context) {
+    const { gameId } = c.req.param();
+    const user = c.get("user");
+
+    if (!user?.id) {
+      throw new HttpException("Unauthorized", 401);
+    }
+
+    if (!VALID_GAME_IDS.includes(gameId as VALID_GAME_IDS_TYPE)) {
+      throw new HttpException("Invalid game id", 400);
+    }
+
+    const currentLevel = await this.dugoutService.getUserGameLevel(
+      gameId,
+      user.id
+    );
+
+    const completedChallenges =
+      currentLevel?.completed_challenges as CompletedChallenges;
+
+    const challenges = await this.dugoutService.getGameLevelChallenges(
+      gameId,
+      (currentLevel?.level ?? "apprentice") as GameLevel
+    );
+
+    const formattedChallenges = challenges.map((challenge) => {
+      return {
+        completed: completedChallenges[
+          currentLevel?.level as keyof CompletedChallenges
+        ].includes(challenge.id),
+        ...challenge,
+      };
+    });
+
+    return sendResponse.success(
+      c,
+      "Challenges fetched successfully",
+      200,
+      formattedChallenges
+    );
+  }
+
+  async completeChallenge(c: Context) {
+    const { gameId } = c.req.param();
+    const user = c.get("user");
+
+    if (!user?.id) {
+      throw new HttpException("Unauthorized", 401);
+    }
+
+    const { challengeId } = c.req.param();
+
+    const currentLevel = await this.dugoutService.getUserGameLevel(
+      gameId,
+      user.id
+    );
+
+    const gameChallenge = await this.dugoutService.getGameChallenge(
+      gameId,
+      (currentLevel?.level ?? "apprentice") as GameLevel,
+      Number(challengeId)
+    );
+
+    if (!gameChallenge) {
+      throw new HttpException("Challenge not found", 404);
+    }
+
+    let result;
+    try {
+      result = await this.dugoutService.completeChallenge(
+        gameId,
+        user.id,
+        (currentLevel?.level ?? "apprentice") as GameLevel,
+        Number(challengeId)
+      );
+    } catch (e: any) {
+      throw new HttpException(e.message, 400);
+    }
+
+    return sendResponse.success(
+      c,
+      "Challenge completed successfully",
+      200,
+      result
     );
   }
 }
