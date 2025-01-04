e2832966064d5d146e24cbd9605a085759b1970d
benrobo <alumonabenaiah71@gmail.com>
2025-01-04 10:30:15 +0100

    added major updates

--- app/src/lib/utils/index.ts
+++ app/src/lib/utils/index.ts
@@ -223,3 +223,22 @@ export 	const calculateReadingTime = (text: string, wpm = 200) => {
     sec: seconds
   };
 }
+
+export const shuffleArray = <T>(array: T[]) => {
+  let currentIndex = array.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex !== 0) {
+    // Pick a remaining element...
+    const randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+};
