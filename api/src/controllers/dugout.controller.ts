3bdc31f578e4b3f700b2f2b5e8a5f5d003c7c11a
benrobo <alumonabenaiah71@gmail.com>
2025-01-05 05:29:15 +0100

    added major updates

--- api/src/controllers/dugout.controller.ts
+++ api/src/controllers/dugout.controller.ts
@@ -3,9 +3,14 @@ import DugoutService from "../services/dugout.service.js";
 import sendResponse from "../lib/send-response.js";
 import { HttpException } from "../lib/exception.js";
 import shortUUID from "short-uuid";
-import { GAME_LEVELS, VALID_GAME_IDS } from "../constant/dugout.js";
+import {
+  GAME_LEVELS,
+  USER_GAME_LEVELS_MAP_TOTAL_POINTS,
+  VALID_GAME_IDS,
+} from "../constant/dugout.js";
 import {
   CompletedChallenges,
+  DugoutUserStats,
   GameLevel,
   VALID_GAME_IDS as VALID_GAME_IDS_TYPE,
 } from "../types/dugout.types.js";
@@ -168,6 +173,54 @@ class DugoutController {
       200,
       result
     );
+  }
+
+  async getUserStats(c: Context) {
+    const user = c.get("user");
+
+    if (!user?.id) {
+      throw new HttpException("Unauthorized", 401);
+    }
+
+    const stats = await this.dugoutService.getUserStats(user.id);
+
+    const getHighestLevel = (stats: DugoutUserStats[]) => {
+      let highestLevel: string | null = null;
+      const levelsOrder = GAME_LEVELS;
+
+      if (stats.length === 0) return highestLevel;
+
+      for (const stat of stats) {
+        if (
+          levelsOrder.indexOf(stat.level) >
+          levelsOrder.indexOf(highestLevel as any)
+        ) {
+          highestLevel = stat.level;
+        }
+      }
+      return highestLevel;
+    };
+
+    return sendResponse.success(c, "Stats fetched successfully", 200, {
+      highest_level: getHighestLevel(stats as any),
+      stats,
+    });
+  }
+
+  async getUserPointsByGameId(c: Context) {
+    const { gameId } = c.req.param();
+    const user = c.get("user");
+
+    if (!user?.id) {
+      throw new HttpException("Unauthorized", 401);
+    }
+
+    const points = await this.dugoutService.getUserPointsByGameId(
+      gameId,
+      user.id
+    );
+
+    return sendResponse.success(c, "Points fetched successfully", 200, points);
   }
 }
 
