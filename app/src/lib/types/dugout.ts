abef89f15de1c3db20d97ab83b8f1c3ad101ebb3
benrobo <alumonabenaiah71@gmail.com>
2025-01-05 05:29:16 +0100

    added major updates

---
+++ app/src/lib/types/dugout.ts
@@ -0,0 +1,63 @@
+export type DugoutGameLevels = 'apprentice' | 'planetary' | 'stellar';
+
+export type DugoutGameProgress = {
+  id: string;
+  user_id: string;
+  dugout_game_id: string;
+  level: string;
+  total_challenges: number;
+  completed_challenges: {
+    stellar: number[];
+    planetary: number[];
+    apprentice: number[];
+  };
+  points: number;
+  created_at: string;
+};
+
+
+export type DugoutUserStats = {
+  highest_level: DugoutGameLevels | null;
+  stats: Array<{
+    points: number;
+    dugout_game_id: string;
+    level: DugoutGameLevels;
+  }>;
+};
+
+export type JoinGameResponse = {
+    id: string;
+    user_id: string;
+    dugout_game_id: string;
+    level: string;
+    total_challenges: number;
+    completed_challenges: {
+        stellar: number[];
+        planetary: number[];
+        apprentice: number[];
+    };
+    points: number;
+    created_at: string;
+}
+
+
+export type FourPicOneWordChallenge = {
+    completed: boolean;
+    id: number;
+    secret: {
+        word: string;
+        display: string;
+    };
+    definition: string;
+    media: {
+        type: string;
+        url: string;
+        description: string;
+    }[]
+};
+
+export type FourPicOneWordGameLevelChallenges = {
+    stellar: FourPicOneWordChallenge[];
+    planetary: FourPicOneWordChallenge[];
+    apprentice: FourPicOneWordChallenge[];
+};
