e6f98ead972071694b9e4fa5a7a40a7d61de5ff2
benrobo <alumonabenaiah71@gmail.com>
2025-01-06 17:20:23 +0100

    added major updates

---
+++ app/src/lib/store/dugout.store.ts
@@ -0,0 +1,36 @@
+import { useLocalStorage } from '@/hooks/useLocalStorage';
+import type { ConversationResponse } from '@/types/chatfeed';
+import { writable } from 'svelte/store';
+
+export type DugoutStore = {
+	userGameLevelSession: {
+		level: string;
+		game_id: string;
+	}[]
+};
+
+// Initial state with required default values
+const initialState: DugoutStore = {
+    userGameLevelSession: []
+};
+
+function createDugoutStore() {
+	const { subscribe, set, update } = writable<DugoutStore>(initialState);
+
+	return {
+		subscribe,
+        setUserGameLevelSession: (session: {
+            level: string;
+            game_id: string;
+        }[]) => update((state) => ({ ...state, userGameLevelSession: session })),
+		reset: () => set(initialState)
+	};
+}
+
+// Create a single instance of the store
+export const dugoutStore = createDugoutStore();
+
+// Export a function that always returns the same store instance
+export const useDugoutStore = () => dugoutStore;
+
+export default createDugoutStore;
