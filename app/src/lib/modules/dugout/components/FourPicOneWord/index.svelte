9f953a5a6fbcfad6e12317ba91ad914229a97913
benrobo <alumonabenaiah71@gmail.com>
2025-01-06 12:51:29 +0100

    added major updates

--- app/src/lib/modules/dugout/components/FourPicOneWord/index.svelte
+++ app/src/lib/modules/dugout/components/FourPicOneWord/index.svelte
@@ -3,21 +3,74 @@
 	import SplashScreen from './SplashScreen.svelte';
 	import GameArea from './GameArea.svelte';
 	import { fade } from 'svelte/transition';
-	import { cn } from '@/utils';
+	import { cn, extractAxiosResponseData } from '@/utils';
 	import { goto } from '$app/navigation';
+	import { createMutation, createQuery } from '@tanstack/svelte-query';
+	import { getGameLevelChallenges, joinGame } from '@/http/requests';
+	import type { FourPicOneWordChallenge } from '@/types/dugout';
+	import { useLocalStorage } from '@/hooks/useLocalStorage';
+
+	export let slug: string;
+
+	$: gameSession = useLocalStorage<{
+		challenges: FourPicOneWordChallenge[];
+		hint_points: number;
+		current_challenge: FourPicOneWordChallenge | null;
+		level: string;
+	}>('4-pic-1-word', {
+		challenges: [],
+		hint_points: 10,
+		current_challenge: null,
+		level: 'level 1'
+	});
 
 	$: showSplashScreen = true;
 	$: showGameArea = false;
 
-	const onHideSplashScreen = () => {
-		showSplashScreen = !showSplashScreen;
-	};
+	$: getGameChallenges = createQuery({
+		queryKey: ['game-challenges', slug],
+		queryFn: () => getGameLevelChallenges(slug)
+	});
+
+	$: joinGameMut = createMutation({
+		mutationFn: (slug: string) => joinGame(slug),
+		onSuccess: () => {
+			showSplashScreen = false;
+			showGameArea = true;
+		},
+		onError: () => {
+			showSplashScreen = true;
+			showGameArea = false;
+		}
+	});
+
+	const onHideSplashScreen = () => {};
 
 	$: {
 		if (!showSplashScreen) {
 			setTimeout(() => {
 				showGameArea = true;
 			}, 500);
+		}
+
+		// game challenges
+		if ($getGameChallenges.data) {
+			const data = extractAxiosResponseData($getGameChallenges.data, 'success')
+				?.data as unknown as FourPicOneWordChallenge[];
+			// save to local storage
+			const sortByCompleted = data?.sort((a: any, b: any) => a?.completed - b?.completed);
+			const currentLevel = sortByCompleted[0]?.id;
+			const filteredNonCompletedChallenges = $gameSession.challenges.filter(
+				(challenge) => !challenge.completed
+			);
+			const shuffledChallenges = filteredNonCompletedChallenges.sort(() => Math.random() - 0.5);
+			gameSession.update((prev) => ({
+				...prev,
+				challenges: data,
+				hint_points: $gameSession?.hint_points,
+				current_challenge: $gameSession?.current_challenge ?? shuffledChallenges[0],
+				level: currentLevel ? `level ${currentLevel}` : 'level 1'
+			}));
 		}
 	}
 
@@ -38,8 +91,13 @@
 	transition:fade={{ duration: 1000 }}
 >
 	{#if showSplashScreen}
-		<div class="w-full h-full" transition:fade={{ duration: 500 }}>
-			<SplashScreen {onHideSplashScreen} />
+		<div class="w-full h-auto" transition:fade={{ duration: 500 }}>
+			<SplashScreen
+				onClick={() => {
+					$joinGameMut.mutate(slug);
+				}}
+				loading={$joinGameMut.isPending || $getGameChallenges.isLoading}
+			/>
 		</div>
 	{/if}
 
