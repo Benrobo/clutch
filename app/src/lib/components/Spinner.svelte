cfedd150d5c7d68f581d38f03ad32475b5600bdf
benrobo <alumonabenaiah71@gmail.com>
2025-01-06 12:51:29 +0100

    added major updates

--- app/src/lib/components/Spinner.svelte
+++ app/src/lib/components/Spinner.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
-  import type { ClassNameValue } from "tailwind-merge";
-  import { cn } from "$lib/utils";
-  import Loader from "lucide-svelte/icons/loader";
+	import type { ClassNameValue } from 'tailwind-merge';
+	import { cn } from '$lib/utils';
+	import Loader from 'lucide-svelte/icons/loader';
 
-  export let className: ClassNameValue = "";
-  export let size: string = "20";
+	export let className: ClassNameValue = '';
+	export let size: string = '20';
+	export let strokeWidth: string = '1.5';
 </script>
 
-<Loader class={cn("animate-spin", className)} {size} />
+<Loader class={cn('animate-spin', className)} {size} {strokeWidth} />
