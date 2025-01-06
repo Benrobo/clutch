054d4aa496e2f569ce3ebd65c9a19420ac430690
benrobo <alumonabenaiah71@gmail.com>
2025-01-06 16:28:25 +0100

    added major updates

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -186,10 +186,27 @@ export default {
 					'20%,50%': { opacity: '0' }
 				}
 			},
+			keyframes: {
+                'shake' : {
+                    '10%, 90%': {
+                        transform: 'translate3d(-1px, 0, 0)'
+                    },
+                    '20%, 80%' : {
+                        transform: 'translate3d(2px, 0, 0)'
+                    },
+                    '30%, 50%, 70%': {
+                        transform: 'translate3d(-4px, 0, 0)'
+                    },
+                    '40%, 60%': {
+                        transform: 'translate3d(4px, 0, 0)'
+                    }
+				}
+			},
 			animation: {
 				'accordion-down': 'accordion-down 0.2s ease-out',
 				'accordion-up': 'accordion-up 0.2s ease-out',
-				'caret-blink': 'caret-blink 1.25s ease-out infinite'
+				'caret-blink': 'caret-blink 1.25s ease-out infinite',
+				'shake': 'shake 0.8s cubic-bezier(.36,.07,.19,.97) both'
 			}
 		}
 	},
