3b0c005a2204e41046940fec3590af642aa6d18b
benrobo <alumonabenaiah71@gmail.com>
2025-01-08 03:27:00 +0100

    added major updates

--- app/src/lib/modules/chat/components/EmptyChat.svelte
+++ app/src/lib/modules/chat/components/EmptyChat.svelte
@@ -5,7 +5,7 @@
 
 <Flex className="w-full h-screen absolute top-0 left-0 flex-center  z-[1]">
 	<Flex className="w-full h-auto flex-center flex-col gap-4 px-10 text-center">
-		<Logo size={80} className="stroke-dark-100 text-red-305 drop-shadow-lg" />
+		<Logo size={80} className="stroke-red-200/10 stroke-[2px] text-red-305 drop-shadow-lg" />
 		<h1 class="text-white-200 font-recoleta font-semibold text-2xl">Your Digital Baseball Mind</h1>
 	</Flex>
 </Flex>
