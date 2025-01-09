86ce8f38edcf51d0bb41d5cf487bac6cd4874e2f
benrobo <alumonabenaiah71@gmail.com>
2025-01-09 22:46:37 +0100

    added major updates

--- app/src/routes/home/matchup/+page.svelte
+++ app/src/routes/home/matchup/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
+	import BottomSheet from '@/components/BottomSheet.svelte';
 	import Flex from '@/components/Flex.svelte';
 	import Input from '@/components/ui/input.svelte';
+	import { GAME_SEASONS } from '@/constant/matchup';
 	import { players, teams } from '@/data/matchup';
 	import ComparePlayers from '@/modules/matchup/components/ComparePlayers.svelte';
 	import Notfound from '@/modules/matchup/components/Notfound.svelte';
@@ -10,10 +12,21 @@
 	import { BadgeCheck, CheckCheck, ListFilter, Scale, Search, X } from 'lucide-svelte';
 	import { afterUpdate } from 'svelte';
 
-	let selectedTeam: number | null = null;
+	let selectedTeam: number = teams[0]?.id;
 
 	let selectedPlayers: Player[] = [];
 	$: selectedPlayers = [];
+
+	let showSearchFilter = false;
+
+	$: seasonValue = '2024';
+
+	let filters: {
+		season: string;
+	};
+	$: filters = {
+		season: '2024'
+	};
 
 	afterUpdate(() => {
 		console.log(selectedPlayers);
@@ -21,7 +34,7 @@
 </script>
 
 <div class="w-full h-screen mx-auto flex flex-col items-center justify-center bg-dark-103">
-	<div class="w-full h-full max-w-[678px] mx-auto">
+	<div class="w-full h-full max-w-[678px] mx-auto relative">
 		<!-- header -->
 		<div class="flex flex-col items-center justify-center gap-2 px-4 md:px-8 py-10">
 			<Flex className="w-full flex-row items-center py-1">
@@ -40,20 +53,30 @@
 					<div
 						class="w-full h-full max-h-[50px] bg-none rounded-lg flex items-center justify-center"
 					>
-						<Search size={20} class="stroke-white-100" />
+						<Search size={25} strokeWidth={3} class="stroke-white-100" />
 						<input
 							type="text"
-							class="w-full h-full text-lg bg-transparent outline-none border-none placeholder:text-white-100 text-white-100 font-bold font-recoleta focus:ring-0"
+							class="w-full h-full text-lg bg-transparent outline-none border-none placeholder:text-white-100 text-white-100 font-bold font-gothic-one focus:ring-0 placeholder:text-white-100/50"
 							placeholder="Search Players"
 						/>
 					</div>
 					<button
 						class={cn(
-							'flex-center w-[45px] h-[42px] p-2 bg-none border-[1px] border-white-200/20 rounded-lg',
+							'flex-center w-[45px] h-[42px] p-2 bg-none border-[1px] border-white-200/20 rounded-lg relative',
 							false ? 'bg-orange-103' : ''
 						)}
+						on:click={() => {
+							showSearchFilter = true;
+						}}
 					>
 						<ListFilter size={20} class="stroke-white-100" />
+
+						<!-- filter indicator -->
+						{#if filters.season !== '2024'}
+							<span
+								class="p-1 rounded-full bg-orange-101 absolute top-0 -translate-y-1 translate-x-1 right-0"
+							/>
+						{/if}
 					</button>
 				</Flex>
 
@@ -101,7 +124,24 @@
 		</div>
 
 		<div class="h-screen -translate-y-10">
-			<ComparePlayers {players} />
+			<ComparePlayers
+				{players}
+				{selectedPlayers}
+				onSelectPlayer={(player) => {
+					const isMax = selectedPlayers.length === 2;
+					const idExists = selectedPlayers.find((p) => p.id === player?.id);
+					if (isMax && !idExists) return;
+
+					if (idExists) {
+						selectedPlayers = selectedPlayers.filter((p) => p.id !== player?.id);
+					} else {
+						selectedPlayers = [...selectedPlayers, player];
+					}
+				}}
+				removeSelectedPlayer={(player) => {
+					selectedPlayers = selectedPlayers.filter((p) => p.id !== player?.id);
+				}}
+			/>
 		</div>
 
 		<!-- empty state when no players are found or no recent matchup -->
@@ -112,5 +152,73 @@
 			showCTA={true}
 			ctaText="Compare Players"
 		/> -->
+		<!-- Finalize Lineup -->
+		{#if true}
+			<div
+				class="w-full h-[100px] flex flex-col items-center justify-center gap-2 absolute left-0 right-0 bottom-0 z-[10] bg-gradient-to-t from-dark-103 to-transparent px-10"
+			>
+				<button
+					class={cn(
+						'w-full max-w-[300px] h-auto  rounded-full px-4 py-3 text-lg font-semibold font-gothic-one enableBounceEffect disabled:cursor-not-allowed',
+						selectedPlayers.length === 2
+							? 'bg-orange-101 text-white-100'
+							: 'bg-dark-106 text-white-100 text-white-100/50'
+					)}
+					disabled={selectedPlayers.length !== 2}
+				>
+					Finalize Lineup
+				</button>
+			</div>
+		{/if}
 	</div>
 </div>
+
+<!-- Search Filter Bottom Sheet -->
+<BottomSheet
+	className="w-full h-auto min-h-[20vh] bg-dark-106"
+	showBackdrop={true}
+	isOpen={showSearchFilter}
+	showCloseButton={true}
+	closeBtnClassName="bg-dark-100/30 text-white-100 stroke-white-100"
+	closeBtnIconClassName="stroke-white-100"
+	showDragHandle={false}
+	onClose={() => {
+		showSearchFilter = false;
+	}}
+>
+	<Flex className="w-full h-full flex flex-col gap-2 px-[4em]">
+		<!-- <h1 class="text-white-100 text-xl font-semibold">Search Filter</h1> -->
+		<Flex className="w-full h-auto flex flex-row items-center justify-between gap-2 py-2">
+			<p class="text-white-100 text-sm font-normal">Seasons</p>
+			<select
+				name=""
+				id=""
+				class="w-full h-full bg-dark-100/30 outline-none border-none ring-0 rounded-lg focus:ring-0 cursor-pointer"
+				value={seasonValue}
+				on:change={(e) => {
+					// @ts-expect-error
+					seasonValue = e.target?.value;
+				}}
+			>
+				{#each GAME_SEASONS as season}
+					<option value={season?.value}>{season?.label}</option>
+				{/each}
+			</select>
+		</Flex>
+
+		<br />
+		<Flex className="w-full flex-center">
+			<button
+				class={cn(
+					'w-full h-auto bg-orange-101 rounded-full px-4 py-3 text-white-100 text-md font-semibold font-gothic-one enableBounceEffect'
+				)}
+				on:click={() => {
+					showSearchFilter = false;
+					filters.season = seasonValue;
+				}}
+			>
+				Apply
+			</button>
+		</Flex>
+	</Flex>
+</BottomSheet>
