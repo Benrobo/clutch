60fe3f5b5c0f795da6053ba8788e7e0e3e5a11e8
benrobo <alumonabenaiah71@gmail.com>
2025-01-09 14:50:23 +0100

    added major updates

---
+++ app/src/lib/types/matchup.ts
@@ -0,0 +1,14 @@
+
+export type Player = {
+    teamId: number;
+    id: number;
+    fullName: string;
+    verified: boolean;
+    active: boolean;
+    position: string;
+    jerseyNumber: string;
+    profilePicture: string;
+    height: string;
+    weight: number;
+    currentAge: number;
+}
