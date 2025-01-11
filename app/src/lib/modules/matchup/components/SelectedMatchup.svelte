a2418ace3a8c67b66b7670b33ef80ecb07961da8
benrobo <alumonabenaiah71@gmail.com>
2025-01-11 17:43:31 +0100

    added major updates

--- app/src/lib/modules/matchup/components/SelectedMatchup.svelte
+++ app/src/lib/modules/matchup/components/SelectedMatchup.svelte
@@ -9,6 +9,20 @@
 	import { afterUpdate } from 'svelte';
 	import VersusOverview from './VersusOverview.svelte';
 	import PlayerProfile from './PlayerProfile.svelte';
+	import { pictcherStats, players, teams } from '@/data/matchup';
+
+	let currentSlideIndex = 0;
+
+	const player1Info = {
+		player: players[Math.floor(Math.random() * players.length)],
+		stats: pictcherStats[0],
+		team: teams[Math.floor(Math.random() * teams.length)]
+	};
+	const player2Info = {
+		player: players[Math.floor(Math.random() * players.length)],
+		stats: pictcherStats[0],
+		team: teams[Math.floor(Math.random() * teams.length)]
+	};
 
 	$: currentSlideIndex = 0;
 </script>
@@ -24,24 +38,46 @@
 	contentClassName="px-0 py-0"
 >
 	<div class="w-full h-screen relative">
-		<!-- <VersusOverview onClose={() => {}} /> -->
-
-		<PlayerProfile next={() => {}} prev={() => {}} />
+		{#if currentSlideIndex === 0}
+			<VersusOverview
+				onClose={() => {}}
+				challenger={player1Info.player}
+				opponent={player2Info.player}
+			/>
+		{:else if currentSlideIndex === 1}
+			<PlayerProfile
+				playerInfo={player2Info.player}
+				playerStats={player2Info.stats}
+				teamInfo={player2Info.team}
+			/>
+		{:else if currentSlideIndex === 2}
+			<PlayerProfile
+				playerInfo={player1Info.player}
+				playerStats={player1Info.stats}
+				teamInfo={player1Info.team}
+			/>
+		{/if}
 
 		<Flex
-			className="w-full h-auto pb-5 items-center justify-between absolute bottom-5 right-0 px-5"
+			className="w-full h-auto pb-2 items-center justify-between absolute bottom-2 right-0 px-5"
 		>
 			<button
 				class={cn(
 					'w-full max-w-[90px] min-h-[45px] bg-dark-111 flex items-center justify-center rounded-full enableBounceEffect border-[1px] border-white-400/30',
 					currentSlideIndex === 0 ? 'invisible' : ''
 				)}
+				on:click={() => {
+					currentSlideIndex -= 1;
+				}}
 			>
 				<MoveLeft size={30} strokeWidth={1} class="stroke-white-200" />
 			</button>
 
 			<button
 				class="w-full max-w-[90px] min-h-[45px] bg-dark-111 flex items-center justify-center rounded-full enableBounceEffect border-[1px] border-white-400/30"
+				on:click={() => {
+					currentSlideIndex += 1;
+				}}
 			>
 				<MoveRight size={30} strokeWidth={1} class="stroke-white-200" />
 			</button>
