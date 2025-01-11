aefb4239958e41acf205907af0a897cd289fb3fc
benrobo <alumonabenaiah71@gmail.com>
2025-01-12 00:16:40 +0100

    added major updates

---
+++ app/src/lib/constant/mlb.ts
@@ -0,0 +1,368 @@
+export const MLB_PLAYER_POSITIONS = [
+  {
+    "shortName": "Pitcher",
+    "abbrev": "P",
+    "type": "Pitcher"
+  },
+  {
+    "shortName": "Catcher",
+    "abbrev": "C",
+    "type": "Catcher"
+  },
+  {
+    "shortName": "First Base",
+    "abbrev": "1B",
+    "type": "Infielder"
+  },
+  {
+    "shortName": "Second Base",
+    "abbrev": "2B",
+    "type": "Infielder"
+  },
+  {
+    "shortName": "Third Base",
+    "abbrev": "3B",
+    "type": "Infielder"
+  },
+  {
+    "shortName": "Shortstop",
+    "abbrev": "SS",
+    "type": "Infielder"
+  },
+  {
+    "shortName": "Left Field",
+    "abbrev": "LF",
+    "type": "Outfielder"
+  },
+  {
+    "shortName": "Center Field",
+    "abbrev": "CF",
+    "type": "Outfielder"
+  },
+  {
+    "shortName": "Right Field",
+    "abbrev": "RF",
+    "type": "Outfielder"
+  },
+  {
+    "shortName": "Designated Hitter",
+    "abbrev": "DH",
+    "type": "Hitter"
+  }
+]
+
+
+export const MLB_STATS_SCHEMA = [
+  {
+    "key": "strikeOuts",
+    "title": "Strikeouts"
+  },
+  {
+    "key": "strikeoutsPer9Inn",
+    "title": "Strikeouts Per 9 Innings"
+  },
+  {
+    "key": "saves",
+    "title": "Saves"
+  },
+  {
+    "key": "saveOpportunities",
+    "title": "Save Opportunities"
+  },
+  {
+    "key": "era",
+    "title": "Earned Run Average"
+  },
+  {
+    "key": "whip",
+    "title": "Walks Plus Hits Per Inning Pitched"
+  },
+  {
+    "key": "wins",
+    "title": "Wins"
+  },
+  {
+    "key": "losses",
+    "title": "Losses"
+  },
+  {
+    "key": "winLossPercentage",
+    "title": "Win-Loss Percentage"
+  },
+  {
+    "key": "gamesPlayed",
+    "title": "Games Played"
+  },
+  {
+    "key": "gamesStarted",
+    "title": "Games Started"
+  },
+  {
+    "key": "completeGames",
+    "title": "Complete Games"
+  },
+  {
+    "key": "shutouts",
+    "title": "Shutouts"
+  },
+  {
+    "key": "inningsPitched",
+    "title": "Innings Pitched"
+  },
+  {
+    "key": "hits",
+    "title": "Hits Allowed"
+  },
+  {
+    "key": "runs",
+    "title": "Runs Allowed"
+  },
+  {
+    "key": "earnedRuns",
+    "title": "Earned Runs Allowed"
+  },
+  {
+    "key": "homeRuns",
+    "title": "Home Runs Allowed"
+  },
+  {
+    "key": "baseOnBalls",
+    "title": "Walks Allowed"
+  },
+  {
+    "key": "intentionalWalks",
+    "title": "Intentional Walks Allowed"
+  },
+  {
+    "key": "hitByPitch",
+    "title": "Hit By Pitch"
+  },
+  {
+    "key": "balks",
+    "title": "Balks"
+  },
+  {
+    "key": "wildPitches",
+    "title": "Wild Pitches"
+  },
+  {
+    "key": "battersFaced",
+    "title": "Batters Faced"
+  },
+  {
+    "key": "eraPlus",
+    "title": "ERA+"
+  },
+  {
+    "key": "fip",
+    "title": "Fielding Independent Pitching"
+  },
+  {
+    "key": "hitsPer9Inn",
+    "title": "Hits Allowed Per 9 Innings"
+  },
+  {
+    "key": "homeRunsPer9Inn",
+    "title": "Home Runs Allowed Per 9 Innings"
+  },
+  {
+    "key": "walksPer9Inn",
+    "title": "Walks Allowed Per 9 Innings"
+  },
+  {
+    "key": "strikeoutsPerWalk",
+    "title": "Strikeouts Per Walk"
+  },
+  {
+    "key": "winsAboveReplacement",
+    "title": "Wins Above Replacement (WAR)"
+  },
+  {
+    "key": "winsAboveAverage",
+    "title": "Wins Above Average (WAA)"
+  },
+  {
+    "key": "qualityStarts",
+    "title": "Quality Starts"
+  },
+  {
+    "key": "inheritedRunners",
+    "title": "Inherited Runners"
+  },
+  {
+    "key": "inheritedRunnersScored",
+    "title": "Inherited Runners Scored"
+  },
+  {
+    "key": "blownSaves",
+    "title": "Blown Saves"
+  },
+  {
+    "key": "savePercentage",
+    "title": "Save Percentage"
+  },
+  {
+    "key": "gamesFinished",
+    "title": "Games Finished"
+  },
+  {
+    "key": "gamesInRelief",
+    "title": "Games in Relief"
+  },
+  {
+    "key": "strikeoutPercentage",
+    "title": "Strikeout Percentage"
+  },
+  {
+    "key": "walkPercentage",
+    "title": "Walk Percentage"
+  },
+  {
+    "key": "strikeoutMinusWalkPercentage",
+    "title": "Strikeout Minus Walk Percentage"
+  },
+  {
+    "key": "pitches",
+    "title": "Total Pitches"
+  },
+  {
+    "key": "pitchesPerGameStarted",
+    "title": "Pitches Per Game Started"
+  },
+  {
+    "key": "strikes",
+    "title": "Strikes"
+  },
+  {
+    "key": "strikePercentage",
+    "title": "Strike Percentage"
+  },
+  {
+    "key": "groundOuts",
+    "title": "Ground Outs"
+  },
+  {
+    "key": "airOuts",
+    "title": "Air Outs"
+  },
+  {
+    "key": "doubles",
+    "title": "Doubles"
+  },
+  {
+    "key": "triples",
+    "title": "Triples"
+  },
+  {
+    "key": "avg",
+    "title": "Batting Average"
+  },
+  {
+    "key": "atBats",
+    "title": "At Bats"
+  },
+  {
+    "key": "obp",
+    "title": "On-Base Percentage"
+  },
+  {
+    "key": "slg",
+    "title": "Slugging Percentage"
+  },
+  {
+    "key": "ops",
+    "title": "On-Base Plus Slugging"
+  },
+  {
+    "key": "caughtStealing",
+    "title": "Caught Stealing"
+  },
+  {
+    "key": "stolenBases",
+    "title": "Stolen Bases"
+  },
+  {
+    "key": "stolenBasePercentage",
+    "title": "Stolen Base Percentage"
+  },
+  {
+    "key": "groundIntoDoublePlay",
+    "title": "Ground Into Double Play"
+  },
+  {
+    "key": "numberOfPitches",
+    "title": "Number of Pitches"
+  },
+  {
+    "key": "plateAppearances",
+    "title": "Plate Appearances"
+  },
+  {
+    "key": "totalBases",
+    "title": "Total Bases"
+  },
+  {
+    "key": "rbi",
+    "title": "Runs Batted In"
+  },
+  {
+    "key": "leftOnBase",
+    "title": "Left On Base"
+  },
+  {
+    "key": "sacBunts",
+    "title": "Sacrifice Bunts"
+  },
+  {
+    "key": "sacFlies",
+    "title": "Sacrifice Flies"
+  },
+  {
+    "key": "babip",
+    "title": "Batting Average on Balls in Play"
+  },
+  {
+    "key": "groundOutsToAirouts",
+    "title": "Ground Outs to Air Outs Ratio"
+  },
+  {
+    "key": "catchersInterference",
+    "title": "Catcher's Interference"
+  },
+  {
+    "key": "atBatsPerHomeRun",
+    "title": "At Bats Per Home Run"
+  },
+  {
+    "key": "pickoffs",
+    "title": "Pickoffs"
+  },
+  {
+    "key": "runsScoredPer9",
+    "title": "Runs Scored Per 9 Innings"
+  },
+  {
+    "key": "pitchesPerInning",
+    "title": "Pitches Per Inning"
+  },
+  {
+    "key": "strikeoutWalkRatio",
+    "title": "Strikeout to Walk Ratio"
+  },
+  {
+    "key": "hitBatsmen",
+    "title": "Hit Batsmen"
+  },
+  {
+    "key": "holds",
+    "title": "Holds"
+  },
+  {
+    "key": "outs",
+    "title": "Outs"
+  },
+  {
+    "key": "gamesPitched",
+    "title": "Games Pitched"
+  }
+]
