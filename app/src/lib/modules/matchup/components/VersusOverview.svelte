d747ace91b0cba466230e1aad38a524d9957845c
benrobo <alumonabenaiah71@gmail.com>
2025-01-11 17:43:31 +0100

    added major updates

--- app/src/lib/modules/matchup/components/VersusOverview.svelte
+++ app/src/lib/modules/matchup/components/VersusOverview.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
 	import Flex from '@/components/Flex.svelte';
-	import { players } from '@/data/matchup';
 	import type { Player } from '@/types/matchup';
-	import { cn } from '@/utils';
 	import { X } from 'lucide-svelte';
+	import { quintInOut } from 'svelte/easing';
+	import { fade } from 'svelte/transition';
 
 	export let onClose: () => void;
-
-	const challenger = players[0];
-	const opponent = players[1];
+	export let challenger: Player;
+	export let opponent: Player;
 </script>
 
-<div class="w-full h-full bg-dark-109 flex flex-col items-center justify-between p-0 relative">
+<div
+	class="w-full h-full bg-dark-109 flex flex-col items-center justify-between p-0 relative"
+	transition:fade={{ duration: 500, easing: quintInOut }}
+>
 	<!-- close btn -->
 	<button
 		class="absolute top-5 right-5 w-[45px] h-[45px] md:w-[55px] md:h-[55px] bg-dark-111 rounded-full flex flex-center enableBounceEffect z-[1]"
