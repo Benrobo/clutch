cda889c4fc69966a03b567a4cafb545d70f5f990
benrobo <alumonabenaiah71@gmail.com>
2025-01-12 21:54:20 +0100

    added major updates

--- app/src/lib/modules/matchup/components/comparison-highlights/PlayerOfTheDay.svelte
+++ app/src/lib/modules/matchup/components/comparison-highlights/PlayerOfTheDay.svelte
@@ -14,7 +14,7 @@
 
 <div class="w-full h-full bg-brown-100 flex flex-col items-start justify-start gap-0 p-0 relative">
 	<!-- player basic info -->
-	<Flex className="w-full h-auto px-5 py-5 items-center justify-between">
+	<Flex className="w-full h-auto px-3 md:px-5 py-5 items-center justify-between">
 		<Flex className="w-auto h-auto items-center justify-center gap-2">
 			<h1 class="text-2xl font-medium font-garamond text-dark-100">{player?.fullName}</h1>
 			{#if player?.verified}
@@ -31,18 +31,23 @@
 		</button>
 	</Flex>
 
-	<Flex className="w-full h-auto px-5 py-5 flex-col gap-0 max-w-[70%] mt-[4em]">
-		<span class="px-4 py-2 rounded-full bg-dark-100 text-xs font-normal font-poppins text-brown-100"
+	<Flex className="w-full h-auto px-3 md:px-5 py-5 flex-col gap-0 max-w-[70%] mt-[1em] md:mt-[4em]">
+		<span
+			class="px-4 py-1 md:py-2 rounded-full bg-dark-100 text-[10px] md:text-xs font-normal font-poppins text-brown-100"
 			>Top Performer</span
 		>
-		<h2 class="text-[4em] font-medium font-garamond text-dark-100">Player <br /> Of The Day</h2>
+		<h2 class="text-[3.4em] md:text-[4em] font-medium font-garamond text-dark-100">
+			Player <br /> Of The Day
+		</h2>
 	</Flex>
 
 	<!-- player image and insight -->
 	<div
-		class="w-full min-h-[100px] h-auto flex flex-row items-center justify-start gap-5 px-5 mt-[5em]"
+		class="w-full min-h-[100px] h-auto flex flex-row items-center justify-start gap-5 px-3 md:px-5 mt-[2em] md:mt-[5em]"
 	>
-		<div class="min-w-[100px] h-[100px] bg-brown-101 rounded-full overflow-hidden">
+		<div
+			class="min-w-[80px] h-[80px] md:min-w-[100px] md:h-[100px] bg-brown-101 rounded-full overflow-hidden"
+		>
 			<img
 				src={player?.profilePicture}
 				alt=""
@@ -52,8 +57,8 @@
 		<Flex className="w-full h-auto flex-col gap-0">
 			<span
 				class={cn(
-					'font-medium font-garamond text-dark-100',
-					reason?.length > 20 ? 'text-md' : 'text-lg'
+					'text-sm md:text-lg font-medium font-garamond text-dark-100',
+					reason?.length > 20 ? 'text-sm' : 'text-lg'
 				)}
 			>
 				{reason}
@@ -65,4 +70,7 @@
 			</span>
 		</Flex>
 	</div>
+
+	<!-- stats -->
+	<Flex className="w-full h-full items-end justify-end"></Flex>
 </div>
