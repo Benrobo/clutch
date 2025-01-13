3a915cbe324f66fd8374b3d86d97fc81bd39ffe9
benrobo <alumonabenaiah71@gmail.com>
2025-01-13 11:29:47 +0100

    added major updates

---
+++ api/src/jobs/comparePlayersHighlight.ts
@@ -0,0 +1,11 @@
+import { inngestClient } from "../config/inngest.js";
+import redis from "../config/redis.js";
+import retry from "async-retry";
+
+export const comparePlayersHighlight = inngestClient.createFunction(
+  { id: "compare-players-highlight" },
+  {
+    event: "process-highlights-video",
+  },
+  async ({ step }) => {}
+);
