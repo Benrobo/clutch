06cc23ac329ee203d34bb7776bfee5c40fe4cf06
benrobo <alumonabenaiah71@gmail.com>
2025-01-13 11:29:47 +0100

    added major updates

--- api/src/services/matchup.service.ts
+++ api/src/services/matchup.service.ts
@@ -1,15 +1,44 @@
 import prisma from "../prisma/index.js";
 
+type GetMatchupParams = {
+  userId: string;
+  challengerTeamId: number;
+  opponentTeamId: number;
+  position: string;
+};
+
+type CreateMatchupParams = {
+  userId: string;
+  challengerId: string;
+  opponentId: string;
+  challengerTeamId: number;
+  opponentTeamId: number;
+  position: string;
+  season: number;
+};
+
 class MatchupService {
-  async createMatchup(userId: string, title: string, ref?: string) {
-    return await prisma.chat.create({
+  async createMatchup(params: CreateMatchupParams) {
+    return await prisma.matchups.create({
       data: {
-        user_id: userId,
-        title,
-        ref,
+        user_id: params.userId,
+        challenger_id: params.challengerId,
+        opponent_id: params.opponentId,
+        challenger_team_id: params.challengerTeamId,
+        opponent_team_id: params.opponentTeamId,
+        position: params.position,
+        season: params.season,
       },
-      include: {
-        messages: true,
+    });
+  }
+
+  async getMatchup(params: GetMatchupParams) {
+    return await prisma.matchups.findFirst({
+      where: {
+        user_id: params.userId,
+        opponent_team_id: params.opponentTeamId,
+        challenger_team_id: params.challengerTeamId,
+        position: params.position,
       },
     });
   }
