03bca70a1bdf6ba113bd868112252348b50ea868
benrobo <alumonabenaiah71@gmail.com>
2025-01-18 22:16:16 +0100

    added translated captions for highlight video

--- app/src/lib/modules/discover/components/highlight-video-playback.svelte
+++ app/src/lib/modules/discover/components/highlight-video-playback.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
 	import Flex from '@/components/Flex.svelte';
 	import Spinner from '@/components/Spinner.svelte';
-	import { cn, getTeamLogoWithBg } from '@/utils';
-	import { Pause, Play, Volume2, VolumeX } from 'lucide-svelte';
+	import { cn, getTeamLogoWithBg, mapTranscript } from '@/utils';
+	import { Captions, CaptionsOff, Cog, Pause, Play, Volume2, VolumeX } from 'lucide-svelte';
 	import { onMount, onDestroy, afterUpdate } from 'svelte';
 	import EngagementBar from './EngagementBar.svelte';
 	import useDetectDevice from '$lib/hooks/useDetectDevice';
 	import { useFeedStore } from '@/store/feed.store';
 	import { useUrlParams } from '@/hooks/useUrlParams';
 	import type { RecommendationData } from '@/types/recommendation';
+	import { SUPPORTED_PLAYBACK_SUBTITLE_LANGUAGES_MAP } from '@/constant/recommendation';
+	import Divider from '@/components/Divider.svelte';
+	import { Value } from '@/components/ui/select';
+	import toast from 'svelte-french-toast';
 
 	export let highlight: RecommendationData | null = null;
 	export let onObServedDataId: (id: string | null) => void = () => {};
@@ -24,13 +28,38 @@
 	$: videoElement;
 	let containerRef: HTMLDivElement;
 	let observer: IntersectionObserver;
-	let muted = true;
+	let muted = false;
+	let isTranscriptAvailable = false;
+	let showVideoSettings: boolean;
+	let selectedSubtitleLanguage: keyof typeof SUPPORTED_PLAYBACK_SUBTITLE_LANGUAGES_MAP;
+	let showCaption: boolean = false;
+	let lastTime = 0;
+
+	let videoTranscript:
+		| {
+				lang: string;
+				translation: {
+					start: number;
+					end: number;
+					text: string;
+				}[];
+		  }[]
+		| null = null;
+
+	let currentCaption: string = '';
+	let currentCaptionIndex = 0;
 
 	$: if (videoElement && $feedStore) {
 		handleVideoState();
 	}
 
 	$: isSafariMobile = false;
+	$: isTranscriptAvailable = highlight?.transcript?.translated !== null;
+	$: selectedSubtitleLanguage = 'en';
+
+	$: if (isTranscriptAvailable) {
+		videoTranscript = mapTranscript(highlight?.transcript!);
+	}
 
 	const MAX_DESCRIPTION_LENGTH = 50;
 	const DEBUG_MODE_VIDEO_URL = 'https://www.w3schools.com/html/mov_bbb.mp4';
@@ -101,6 +130,49 @@
 		});
 	}
 
+	function updateCaptions(event: Event) {
+		const currentTime = (event.target as HTMLVideoElement).currentTime;
+		const captions =
+			videoTranscript?.find((transcript) => transcript.lang === selectedSubtitleLanguage)
+				?.translation ?? [];
+
+		// Detect if the video has looped (currentTime < lastTime)
+		if (currentTime < lastTime) {
+			resetCaptions(); // Reset captions when the video loops
+		}
+		lastTime = currentTime;
+
+		// Check if the current time falls within a caption segment
+		if (
+			currentCaptionIndex < captions.length &&
+			currentTime >= captions[currentCaptionIndex].start &&
+			currentTime < captions[currentCaptionIndex].end
+		) {
+			currentCaption = captions[currentCaptionIndex].text;
+		} else {
+			// Move to the next caption if the current time exceeds the end timestamp
+			if (
+				currentCaptionIndex < captions.length &&
+				currentTime >= captions[currentCaptionIndex].end
+			) {
+				currentCaptionIndex++;
+			}
+
+			// Clear the captions if no segment matches the current time
+			if (
+				currentCaptionIndex >= captions.length ||
+				currentTime < captions[currentCaptionIndex].start
+			) {
+				currentCaption = '';
+			}
+		}
+	}
+
+	const resetCaptions = () => {
+		currentCaptionIndex = 0;
+		currentCaption = '';
+	};
+
 	onMount(() => {
 		observer = new IntersectionObserver(handleVisibilityChange, {
 			threshold: 0.7
@@ -110,8 +182,6 @@
 			observer.observe(containerRef);
 		}
 	});
-
-	afterUpdate(() => {});
 
 	onDestroy(() => {
 		if (observer) {
@@ -135,51 +205,132 @@
 			<video
 				bind:this={videoElement}
 				class={cn(
-					'w-full h-full min-h-[450px] object-cover',
-					$feedStore.isInitialVideoLoading ? 'hidden' : 'flex'
+					'w-full h-full min-h-[450px] object-cover bg-dark-100'
+					// $feedStore.isInitialVideoLoading ? 'hidden' : 'flex'
 				)}
 				playsinline
 				preload="auto"
-				muted={true}
+				muted={false}
 				loop={true}
 				width="640"
 				height="360"
 				autoplay
+				poster={highlight?.thumbnail?.main || highlight?.thumbnail?.fallback}
 				on:loadstart={handleLoadStart}
 				on:canplay={handleCanPlay}
 				on:waiting={handleWaiting}
 				on:playing={handlePlaying}
-				on:timeupdate={() => {}}
+				on:timeupdate={updateCaptions}
 				on:error={(e) => {
 					console.log('Video error: ', e);
 				}}
-				poster={highlight?.thumbnail?.main || highlight?.thumbnail?.fallback}
+				on:ended={resetCaptions}
+				on:seeked={resetCaptions}
 			>
 				<source src={highlight?.playback?.mlbVideoUrl} type="video/mp4" />
 				<!-- <source src={DEBUG_MODE_VIDEO_URL} type="video/mp4" /> -->
 			</video>
 		{/if}
 
+		<!-- caption container -->
+		<div
+			class="w-full h-auto absolute bottom-3 left-0 flex flex-row items-center justify-center px-4"
+		>
+			{#if currentCaption && showCaption}
+				<span
+					class="text-md px-2 py-1 rounded-sm bg-dark-100/60 backdrop-blur-md text-center font-poppins"
+				>
+					{currentCaption}
+				</span>
+			{/if}
+		</div>
+
 		<!-- Main Minor Video Control -->
 		<div
 			class={cn(
-				'w-full h-auto px-4 py-3 z-[10] absolute top-0 right-0 flex flex-row items-center justify-between',
-				$feedStore.isInitialVideoLoading ? 'hidden' : 'flex'
+				'w-full h-auto px-4 py-3 z-[10] absolute top-0 right-0 flex flex-row items-center justify-between'
+				// $feedStore.isInitialVideoLoading ? 'hidden' : 'flex'
 			)}
 		>
+			<Flex className="w-auto flex items-center justify-center gap-2 relative">
+				<button
+					class="w-8 h-8 rounded bg-none backdrop-blurrr-xs text-white-100 text-sm hover:bg-white-100/40 flex-center"
+					on:click={() => {
+						muted = !muted;
+						if (videoElement) {
+							videoElement.muted = muted;
+						}
+					}}
+				>
+					{#if muted}
+						<VolumeX size={18} />
+					{:else}
+						<Volume2 size={18} />
+					{/if}
+				</button>
+
+				<button
+					class="w-8 h-8 rounded bg-none backdrop-blurrr-xs text-white-100 text-sm hover:bg-white-100/40 flex-center enableBounceEffect"
+					on:click={() => {
+						showVideoSettings = !showVideoSettings;
+					}}
+				>
+					<Cog size={18} />
+				</button>
+
+				<!-- settings container -->
+				{#if showVideoSettings}
+					<div
+						class="w-auto min-w-[200px] h-auto absolute top-10 left-0 bg-white-100/50 backdrop-blur-md rounded-sm p-2"
+					>
+						<Flex className="w-full h-full flex-col items-start justify-start gap-1">
+							<select
+								name=""
+								id=""
+								class="w-full h-auto text-xs bg-transparent ring-0 outline-none border-none focus:border-none focus:ring-0"
+								on:change={(e) => {
+									showVideoSettings = false;
+									// @ts-expect-error
+									const lang = e.target.value;
+									const transcript = videoTranscript?.find(
+										(transcript) => transcript.lang === lang
+									);
+									if (!transcript) {
+										toast.error('language not available');
+										return;
+									}
+
+									if (!showCaption) showCaption = true;
+									selectedSubtitleLanguage = lang;
+								}}
+								value={selectedSubtitleLanguage}
+								disabled={!isTranscriptAvailable}
+							>
+								{#each Object.entries(SUPPORTED_PLAYBACK_SUBTITLE_LANGUAGES_MAP) as [key, value]}
+									<option value={key}>{value}</option>
+								{/each}
+							</select>
+							<Divider className="h-[1px] bg-dark-100/10" />
+						</Flex>
+					</div>
+				{/if}
+			</Flex>
+
 			<button
-				class="w-8 h-8 rounded bg-none backdrop-blurrr-xs text-white-100 text-sm hover:bg-white-100/40 flex-center"
+				class={cn(
+					'w-6 h-6 rounded bg-none backdrop-blur-xs text-white-100 text-sm bg-white-100/40 flex-center enableBounceEffect',
+					!isTranscriptAvailable && 'opacity-70',
+					showCaption && 'bg-brown-100 text-dark-100'
+				)}
 				on:click={() => {
-					muted = !muted;
-					if (videoElement) {
-						videoElement.muted = muted;
-					}
+					showCaption = !showCaption;
 				}}
+				disabled={!isTranscriptAvailable}
 			>
-				{#if muted}
-					<VolumeX size={18} />
+				{#if isTranscriptAvailable}
+					<Captions size={18} />
 				{:else}
-					<Volume2 size={18} />
+					<CaptionsOff size={18} />
 				{/if}
 			</button>
 			<!-- Aspect ratio toggle -->
