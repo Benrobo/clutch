e480bbb7d2bf50eaa0ab77a753ec6030f45f9558
benrobo <alumonabenaiah71@gmail.com>
2025-01-18 22:16:17 +0100

    added translated captions for highlight video

---
+++ app/src/lib/constant/recommendation.ts
@@ -0,0 +1,8 @@
+export const SUPPORTED_PLAYBACK_SUBTITLE_LANGUAGES = ["en", "es", "ja", "ko"];
+
+export const SUPPORTED_PLAYBACK_SUBTITLE_LANGUAGES_MAP = {
+	en: "English",
+	es: "Spanish",
+	ja: "Japanese",
+	ko: "Korean"
+};
