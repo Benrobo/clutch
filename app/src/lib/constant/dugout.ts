53a94e8d3eb0fe81bc09610ff5df05f3ee933b8f
benrobo <alumonabenaiah71@gmail.com>
2025-01-20 19:45:28 +0100

    added major updates

--- app/src/lib/constant/dugout.ts
+++ app/src/lib/constant/dugout.ts
@@ -3,3 +3,18 @@ export const USER_GAME_LEVELS_MAP_TOTAL_POINTS = {
   planetary: 100,
   stellar: 200,
 };
+
+export const USER_GAME_LEVELS = [
+  {
+    level: "apprentice",
+    total_points: 50,
+  },
+  {
+    level: "planetary",
+    total_points: 100,
+  },
+  {
+    level: "stellar",
+    total_points: 200,
+  },
+];
