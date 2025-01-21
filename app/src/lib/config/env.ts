c363b364a783bed72f4b3ea8427a417fce537f74
benrobo <alumonabenaiah71@gmail.com>
2025-01-21 20:20:49 +0100

    updated

--- app/src/lib/config/env.ts
+++ app/src/lib/config/env.ts
@@ -3,7 +3,7 @@ import { DEV } from "esm-env";
 const inDev: boolean = DEV;
 
 const env = {
-  apiUrl: DEV ? "http://localhost:4050" : "https://benlab.space/clutch-api"
+  apiUrl: DEV ? "http://localhost:4050" : "https://clutch-api.benlab.space"
 };
 
 export default env;
