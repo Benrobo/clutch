3b58a4a43d6c34f326dc571f1ec64230ccd5f7bb
benrobo <alumonabenaiah71@gmail.com>
2025-01-24 19:06:05 +0100

    updated bottom nav

--- app/src/lib/components/BottomNavTab.svelte
+++ app/src/lib/components/BottomNavTab.svelte
@@ -94,7 +94,7 @@
 			<Flex
 				className={cn(
 					'w-full py-3 border-t-[1px] border-t-gray-100 flex-row items-start justify-between',
-					(displayMode === 'standalone' || displayMode === 'twa') && 'pb-6'
+					(displayMode === 'standalone' || displayMode === 'twa') && 'pb-[2em] pt-[1.2em]'
 				)}
 			>
 				{#each tabs as tab}
